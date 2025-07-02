aa478c6a8e57f01db88bd95fa0779cebfa946fb5
Arpit Singh <arpitbuilds@gmail.com>
2025-07-03 04:03:35 +0530

    Time: 0 ms (100%), Space: 7.8 MB (74.83%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int help(int n){
+        int ans=0;
+        int temp=0;
+        while(n){
+            temp=n%10;
+            ans+=(temp*temp);
+            n=n/10;
+        }
+        return ans;
+    }
+    bool isHappy(int n) {
+        int slow=n;
+        int fast=n;
+        do{
+            slow=help(slow);
+            fast=help(help(fast));
+        }while(slow!=fast);
+        return slow==1;
+    }
+};
