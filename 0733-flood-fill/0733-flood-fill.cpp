8dacf2053407e4c502fc6ddf150c34a28148c9a4
Arpit Singh <arpitbuilds@gmail.com>
2025-08-10 16:20:15 +0530

    Time: 0 ms (100%), Space: 18.1 MB (76.7%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    void dfs(int sr,int sc,int inic,vector<vector<int>>& ans,vector<vector<int>>& img,int color){
+    void dfs(int inic,int sr,int sc,vector<vector<int>>& img,vector<vector<int>>& ans,int color){
         ans[sr][sc]=color;
         int r=sr;
         int c=sc;
         int m=img.size();
         int n=img[0].size();
-      int drow[]={-1,0,1,0};
+          int drow[]={-1,0,1,0};
       int dcol[]={0,1,0,-1};
-      for(int i=0;i<4;i++){
-        int nrow=r+drow[i];
-        int ncol=c+dcol[i];
-        if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && ans[nrow][ncol]!=color && img[nrow][ncol]==inic ){
-            dfs(nrow,ncol,inic,ans,img,color);
-        }
-      }
+        for(int i=0;i<4;i++){
+            int nrow=r+drow[i];
+            int ncol=c+dcol[i];
+            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && img[nrow][ncol]==inic && ans[nrow][ncol]!=color){
+                dfs(inic,nrow,ncol,img,ans,color);
 
+            }
+        }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         vector<vector<int>>ans=image;
         int inic=image[sr][sc];
-        dfs(sr,sc,inic,ans,image,color);
+        dfs(inic,sr,sc,image,ans,color);
         return ans;
     }
 };
