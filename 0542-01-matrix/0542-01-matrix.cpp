5076d04b1b7dd6ecc4c9386237494b4a5ceef40d
Arpit Singh <arpitbuilds@gmail.com>
2025-08-10 17:33:49 +0530

    Time: 10 ms (88.94%), Space: 38.8 MB (44.21%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,34 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int n=mat.size();
-        int m=mat[0].size();
-        vector<vector<int>>v(n,vector<int>(m,0));
-        vector<vector<int>>dis(n,vector<int>(m,0));
-        queue<pair<pair<int,int>,int>>q;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(mat[i][j]==0){
-                    v[i][j]=1;;
-                    q.push({{i,j},0});
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<vector<int>>vis(m,vector<int>(n,0));
+         vector<vector<int>>dis(m,vector<int>(n,0));
 
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==0){
+                    vis[i][j]=1;
+                    q.push({{i,j},0});
                 }
             }
         }
-          int delr[] = {-1, 0, 1, 0};
-        int delc[] = {0, 1, 0, -1};
+      int drow[]={-1,0,1,0};
+      int dcol[]={0,1,0,-1};
         while(!q.empty()){
             int r=q.front().first.first;
             int c=q.front().first.second;
             int d=q.front().second;
-            q.pop();
             dis[r][c]=d;
+            q.pop();
             for(int i=0;i<4;i++){
-                int nrow=r+delr[i];
-                int ncol=c+delc[i];
-                if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && v[nrow][ncol]==0 ){
-                    v[nrow][ncol]=1;
+               int nrow=r+drow[i];
+                int ncol=c+dcol[i];
+                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n &&vis[nrow][ncol]==0){
                     q.push({{nrow,ncol},d+1});
+                    vis[nrow][ncol]=1;
+
                 }
             }
         }
