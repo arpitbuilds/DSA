18402b4a9037252d7b7abebcb11bd84080026718
Arpit Singh <arpitbuilds@gmail.com>
2025-09-01 21:58:00 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.5%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     int climbStairs(int n) {
-    vector<int>dp(n+1,-1);
-      if(n<=2){
-        return n;
-    }
-    if(dp[n]!=-1){
-        return dp[n];
-    }
-    return dp[n]=climbStairs(n-1)+climbStairs(n-2);
+     int prev2 = 1; // dp[0]
+        int prev1 = 1; // dp[1]
+
+        for (int i = 2; i <= n; i++) {
+            int curr = prev1 + prev2; 
+            prev2 = prev1; 
+            prev1 = curr; 
+        }
+
+        return prev1; 
     }
 };
