ee6d4ad0abc6efb73a9565c5e7c5fe0b182e4741
Arpit Singh <arpitbuilds@gmail.com>
2025-09-13 13:58:44 +0530

    Time: 39 ms (95.25%), Space: 28.9 MB (99%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,46 +1,34 @@
 class Solution {
 public:
-    vector<vector<int>> result;
-
-    void twoSum(vector<int>& nums, int target, int i, int j) {
-        while (i < j) {
-            int sum = nums[i] + nums[j];
-
-            if (sum > target) {
-                j--;
-            }
-            else if (sum < target) {
-                i++;
-            }
-            else {
-                result.push_back({-target, nums[i], nums[j]});
-
-                // Skip duplicates from left side
-                while (i < j && nums[i] == nums[i+1]) i++;
-                // Skip duplicates from right side
-                while (i < j && nums[j] == nums[j-1]) j--;
-
-                i++;
-                j--;
-            }
-        }
-    }
-
     vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;   // local, no need to clear
         int n = nums.size();
         if (n < 3) return {};
 
-        result.clear();
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i-1]) continue; // skip duplicate n1
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate n1
 
-            int n1 = nums[i];
-            int target = -n1;
+            int target = -nums[i];
+            int l = i + 1, r = n - 1;
 
-            // find pairs that sum to target
-            twoSum(nums, target, i+1, n-1);
+            while (l < r) {
+                int sum = nums[l] + nums[r];
+                if (sum > target) {
+                    r--;
+                } else if (sum < target) {
+                    l++;
+                } else {
+                    result.push_back({nums[i], nums[l], nums[r]});
+
+                    while (l < r && nums[l] == nums[l + 1]) l++; // skip dup n2
+                    while (l < r && nums[r] == nums[r - 1]) r--; // skip dup n3
+
+                    l++;
+                    r--;
+                }
+            }
         }
         return result;
     }
