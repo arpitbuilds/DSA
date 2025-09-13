7412b649329b4e34a3b39bb3994be337f4238846
Arpit Singh <arpitbuilds@gmail.com>
2025-09-13 14:11:26 +0530

    Time: 35 ms (98.33%), Space: 28.9 MB (89.7%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,30 +1,35 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> result;   // local, no need to clear
-        int n = nums.size();
-        if (n < 3) return {};
-
-        sort(nums.begin(), nums.end());
-
-        for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; // skip duplicate n1
-
-            int target = -nums[i];
-            int l = i + 1, r = n - 1;
-
-            while (l < r) {
-                int sum = nums[l] + nums[r];
-                if (sum > target) {
+        vector<vector<int>>result;
+        int n=nums.size();
+        if(n<3){
+            return {};
+        }
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
+            int target=-nums[i];
+            int l=i+1;
+            int r=n-1;
+            while(l<r){
+                int sum=nums[l]+nums[r];
+                if(sum>target){
                     r--;
-                } else if (sum < target) {
+                }
+                else if(sum<target){
                     l++;
-                } else {
-                    result.push_back({nums[i], nums[l], nums[r]});
-
-                    while (l < r && nums[l] == nums[l + 1]) l++; // skip dup n2
-                    while (l < r && nums[r] == nums[r - 1]) r--; // skip dup n3
-
+                }
+                else{
+                    result.push_back({nums[i],nums[l],nums[r]});
+                    while(l<r && nums[l]==nums[l+1]){
+                        l++;
+                    }
+                    while(l<r && nums[r]==nums[r-1]){
+                        r--;
+                    }
                     l++;
                     r--;
                 }
