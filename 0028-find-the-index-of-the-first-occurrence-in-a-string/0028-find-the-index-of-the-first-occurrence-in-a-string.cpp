ce20c5ebd02a849e20404c0a90c00d07a6c27762
Arpit Singh <arpitbuilds@gmail.com>
2025-09-18 00:48:21 +0530

    Time: 0 ms (100%), Space: 8.5 MB (81.42%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,13 +1,9 @@
 class Solution {
 public:
     int strStr(string s1, string s2) {
-        int ans=-1;
         if(s1.find(s2)!=string::npos){
-         ans=s1.find(s2);
+         return s1.find(s2);
         }
-        else{
-            return -1;
-        }
-        return ans;
+        return -1;
     }
 };
