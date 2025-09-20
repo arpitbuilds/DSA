8b831e1dfee8069311269e4000d7f4aa54415d4c
Arpit Singh <arpitbuilds@gmail.com>
2025-09-20 16:11:35 +0530

    Time: 71 ms (57.77%), Space: 14.3 MB (73.1%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    // int solve(int ind, int prev, vector<int>& nums, vector<vector<int>>& dp) {
-    //     int n = nums.size();
-    //     if (ind == n) {
-    //         return 0;
-    //     }
-    //     if (dp[ind][prev + 1] != -1) {
-    //         return dp[ind][prev + 1];
-    //     }
-    //     int len = 0;
-    //     len = 0 + solve(ind + 1, prev, nums, dp);
-    //     if (prev == -1 || nums[ind] > nums[prev]) {
-    //         len = max(len, 1 + solve(ind + 1, ind, nums, dp));
-    //     }
-    //     return dp[ind][prev + 1] = len;
-    // }
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
-        for (int ind = n - 1; ind >= 0; ind--) {
-            for (int prev = ind - 1; prev >= -1; prev--) {
-                int len = 0;
-                len = 0 + dp[ind + 1][prev+1];
-                if (prev == -1 || nums[ind] > nums[prev]) {
-         len=max(len,1+dp[ind+1][ind+1]);
+//    int solve(int ind, int prev, vector<int>& nums, vector<vector<int>>& dp) {
+//         int n = nums.size();
+//         if (ind == n) {
+//             return 0;
+//         }
+//         if (dp[ind][prev + 1] != -1) {
+//             return dp[ind][prev + 1];
+//         }
+//         int ntake = 0 + solve(ind + 1, prev, nums, dp);
+//         int take=0;
+//         if (prev == -1 || nums[ind] > nums[prev]) {
+//          take =  1 + solve(ind + 1, ind, nums, dp);
+//         }x
+//         return dp[ind][prev + 1] = max(take,ntake);
+//     }
+    int lengthOfLIS(vector<int>& arr) {
+        // int n=nums.size();
+        // vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        // return solve(0,-1,nums,dp);
+        int n=arr.size();
+        vector<int>dp(n,1);
+        int maxi=1;
+        for(int i=0;i<n;i++){
+            for(int prev=0;prev<i;prev++){
+                if(arr[prev] <arr[i]){
+                    dp[i]=max(dp[i],1+dp[prev]);
                 }
-                dp[ind][prev + 1] = len;
             }
+            maxi=max(dp[i],maxi);
         }
-        return dp[0][-1+1];
+        return maxi;
     }
 };
