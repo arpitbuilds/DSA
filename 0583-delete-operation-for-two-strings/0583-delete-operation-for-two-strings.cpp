dd8e8c56d109841d8d7e5a3f5feb9c6900f81ec6
Arpit Singh <arpitbuilds@gmail.com>
2025-09-22 23:01:54 +0530

    Time: 14 ms (40.91%), Space: 17.1 MB (21.57%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
-   int solve(int m,int n,string s,string t, vector<vector<int>>&dp){
-        if(m==0 || n==0){
-            return 0;
+ int longestCommonSubsequence(string s, string t) {
+        int m = s.size();
+        int n = t.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        for (int i = 0; i <= m; i++) { 
+            dp[i][0] = 0;
         }
-        if(dp[m][n]!=-1){
-            return dp[m][n];
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = 0;
         }
-        if(s[m-1]==t[n-1]){
-            return dp[m][n]=1+solve(m-1,n-1,s,t,dp);
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                }
+                else{
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
         }
-        return dp[m][n]=max(solve(m-1,n,s,t,dp),solve(m,n-1,s,t,dp));
+
+        return dp[m][n];
     }
     int minDistance(string word1, string word2) {
-          int w1 = word1.size(),w2 = word2.size(); 
-        vector<vector<int>>dp(w1+1,vector<int>(w2+1,-1));
-        int n = solve(w1,w2,word1,word2,dp); 
+        int n = longestCommonSubsequence(word1,word2); 
+        int w1=word1.size();
+        int w2=word2.size();
         return w1+w2 - 2*n; 
         
     }
