56916a0d268dd8a584f030919811686059cc0948
Arpit Singh <arpitbuilds@gmail.com>
2025-09-23 14:31:32 +0530

    Time: 5 ms (72.42%), Space: 13.2 MB (49.03%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,15 +1,15 @@
 class Solution {
     int solve(int index1,int index2,string &word1,string &word2,vector<vector<int>> &dp){
-        if(index1<0){
-            return index2+1;
+        if(index1==0){
+            return index2;
         }
-        if(index2<0){
-            return index1+1;
+        if(index2==0){
+            return index1;
         }
         if(dp[index1][index2]!=-1){
             return dp[index1][index2];
         }
-        if(word1[index1]==word2[index2]){
+        if(word1[index1-1]==word2[index2-1]){
             return 0+solve(index1-1,index2-1,word1,word2,dp);
         }
         int insertion=1+solve(index1,index2-1,word1,word2,dp);
@@ -22,6 +22,6 @@ public:
         int n1=word1.size();
         int n2=word2.size();
         vector<vector<int>> dp(n1+1,vector<int>(n2+1,-1));
-        return solve(n1-1,n2-1,word1,word2,dp);
+        return solve(n1,n2,word1,word2,dp);
     }
 };
