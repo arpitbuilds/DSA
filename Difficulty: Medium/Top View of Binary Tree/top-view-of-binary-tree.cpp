00eaa85b2638a318e4d543cbc011f64085095b13
Arpit Singh <arpitbuilds@gmail.com>
2025-10-08 00:55:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
+++ Difficulty: Medium/Top View of Binary Tree/top-view-of-binary-tree.cpp
@@ -17,27 +17,33 @@ class Solution {
   public:
     vector<int> topView(Node *root) {
         // code here
-   map<int,int>mpp;
-   vector<int>ans;
-   queue<pair<Node*,int>>q;
-   q.push({root,0});
-   while(!q.empty()){
-       int line=q.front().second;
-       Node*root=q.front().first;
-       q.pop();
-       if(mpp.find(line)==mpp.end()){
-           mpp[line]=root->data;
-       }
-       if(root->left){
-           q.push({root->left,line-1});
-       }
-       if(root->right){
-           q.push({root->right,line+1});
-       }
-   }
-        for(auto it :mpp){
+        vector<int>ans;
+        if(root==NULL){
+            return ans;
+        }
+        queue<pair<Node*,int>>q;
+        q.push({root,0});
+        map<int,int>mpp;
+        while(!q.empty()){
+            int line=q.front().second;
+            Node*x=q.front().first;
+            q.pop();
+            if(mpp.find(line)==mpp.end()){
+                mpp[line]=x->data;
+            }
+            if(x->left){
+                q.push({x->left,line-1});
+            }
+            if(x->right){
+                q.push({x->right,line+1});
+            }
+        }
+        for(auto it:mpp){
             ans.push_back(it.second);
+            
         }
         return ans;
+        
+        
     }
 };
