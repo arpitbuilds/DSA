c5617f9dc351783f13717145c09d33b227b62233
Arpit Singh <arpitbuilds@gmail.com>
2025-10-28 14:47:48 +0530

    Time: 2 ms (49.16%), Space: 12.9 MB (14.69%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -5,7 +5,7 @@ public:
         int n=nums2.size();
         unordered_map<int, int> mpp;
         for(int i=n-1;i>=0;i--){
-            while(!st.empty() && st.top()<nums2[i]){
+            while(!st.empty() && st.top()<=nums2[i]){
                 st.pop();
             }
             if(st.empty()){
