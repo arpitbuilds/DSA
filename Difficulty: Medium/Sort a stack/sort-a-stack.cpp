d3e04f09d9f485a3cc1e91031bafcfe058f4d930
Arpit Singh <arpitbuilds@gmail.com>
2025-10-31 23:26:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a stack/sort-a-stack.cpp
+++ Difficulty: Medium/Sort a stack/sort-a-stack.cpp
@@ -1,15 +1,23 @@
 class Solution {
   public:
+    void insert(stack<int>&st,int x){
+        if(st.empty()  || st.top() <= x){
+            st.push(x);
+            return;
+        }
+        int t=st.top();
+        st.pop();
+        insert(st,x);
+        st.push(t);
+    }
     void sortStack(stack<int> &st) {
         // code here
-       priority_queue<int,vector<int>,greater<int>>pq;
-        while(!st.empty()){
-            pq.push(st.top());
-            st.pop();
+        if(st.empty()){
+            return;
         }
-        while(!pq.empty()){
-            st.push(pq.top());
-            pq.pop();
-        }
+        int top=st.top();
+        st.pop();
+        sortStack(st);
+        insert(st,top);
     }
 };
