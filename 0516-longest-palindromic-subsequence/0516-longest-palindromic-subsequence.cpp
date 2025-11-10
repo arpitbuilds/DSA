f7749ac9658adca721517ae2fa7be700ee3c1ff9
Arpit Singh <arpitbuilds@gmail.com>
2025-11-11 00:09:12 +0530

    Time: 95 ms (21.47%), Space: 76.1 MB (20.78%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,11 +1,10 @@
 class Solution {
 public:
-
-      int longestCommonSubsequence(string s, string t) {
-         int m = s.size();
-        int n = t.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
-        for (int i = 0; i <= m; i++) { 
+        int longestCommonSubsequence(string t1, string t2) {
+        int m = t1.size();
+        int n = t2.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 0; i <= m; i++) {
             dp[i][0] = 0;
         }
         for (int j = 0; j <= n; j++) {
@@ -13,20 +12,20 @@ public:
         }
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
-                if (s[i - 1] == t[j - 1]) {
+                if (t1[i - 1] == t2[j - 1]) {
                     dp[i][j] = 1 + dp[i - 1][j - 1];
                 }
                 else{
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        dp[i][j]=0+max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-
         return dp[m][n];
     }
     int longestPalindromeSubseq(string s) {
-          string t=s;
-          reverse(s.begin(),s.end());
-          return longestCommonSubsequence(s,t);
+        string s1=s;
+        reverse(s.begin(),s.end());
+        return longestCommonSubsequence(s,s1);
+
     }
 };
