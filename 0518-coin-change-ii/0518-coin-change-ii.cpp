28583dad33a6abdf51e4b6ec56008030cba474c5
Arpit Singh <arpitbuilds@gmail.com>
2025-11-10 14:04:14 +0530

    Time: 27 ms (58.48%), Space: 36.7 MB (42.04%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,39 +1,27 @@
 class Solution {
 public:
-    // int solve(int ind,int amount,vector<int>&coins,vector<vector<int>>&dp){
-    //     if(ind==0){
-    //         if(amount%coins[0]==0){
-    //             return 1;
-    //         }
-    //         return 0;
-    //     }
-    //     if(dp[ind][amount]!=-1){
-    //         return dp[ind][amount];
-    //     }
-    //     int ntake=solve(ind-1,amount,coins,dp);
-    //     int take=0;
-    //     if(coins[ind]<=amount){
-    //         take=solve(ind,amount-coins[ind],coins,dp);
-    //     }
-    //   return dp[ind][amount]=take+ntake;
-    // }
+    int solve(int ind,int amt,vector<int>&coins,vector<vector<int>>&dp){
+        if(ind==0){
+            if(amt%coins[ind]==0){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        if(dp[ind][amt]!=-1){
+            return dp[ind][amt];
+        }
+        int ntake=solve(ind-1,amt,coins,dp);
+        int take=0;
+        if(amt>=coins[ind]){
+            take=solve(ind,amt-coins[ind],coins,dp);
+        }
+        return dp[ind][amt]=take+ntake;
+    }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
-        vector<int>prev(amount+1,0),cur(amount+1,0);
-        for(int i=0;i<=amount;i++){
-            if (i% coins[0] == 0) prev[i] = 1;
-        }
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=amount;j++){
-            long long ntake=prev[j];
-            long long take=0;
-            if(coins[i]<=j){
-                take=cur[j-coins[i]];
-            }
-            cur[j]=take+ntake;
-            }
-            prev=cur;
-        }
-        return prev[amount];
+        vector<vector<int>>dp(n,vector<int>(amount+1,-1));
+        return solve(n-1,amount,coins,dp);
     }
 };
