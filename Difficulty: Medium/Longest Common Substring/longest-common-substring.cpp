6457cdb8b53bea56c0a0ff9148192fb3bda4da0f
Arpit Singh <arpitbuilds@gmail.com>
2025-11-11 01:07:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
+++ Difficulty: Medium/Longest Common Substring/longest-common-substring.cpp
@@ -1,31 +1,32 @@
 class Solution {
-public:
-    int solve(int m, int n, string &s, string &t, vector<vector<int>>& dp, int &ans) {
-        if (m == 0 || n == 0) return 0;
-
-        if (dp[m][n] != -1) return dp[m][n];
-
-        if (s[m - 1] == t[n - 1]) {
-            dp[m][n] = 1 + solve(m - 1, n - 1, s, t, dp, ans);
-            ans = max(ans, dp[m][n]);   // update max length
-            return dp[m][n];
-        } else {
-            return dp[m][n] = 0;  // reset because substring must be contiguous
+  public:
+    int solve(int i,int j ,string s1,string s2,vector<vector<int>>&dp,int &ans){
+        if(i==0 || j==0){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(s1[i-1]==s2[j-1]){
+            dp[i][j]=1+solve(i-1,j-1,s1,s2,dp,ans);
+            ans=max(ans,dp[i][j]);
+            return dp[i][j];
+        }
+        else{
+            return dp[i][j]=0;
         }
     }
-
     int longestCommonSubstr(string& s1, string& s2) {
-        int m = s1.size(), n = s2.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
-        int ans = 0;
-
-        // try all ending positions
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                solve(i, j, s1, s2, dp, ans);
+        // your code here
+        int m=s1.size();
+        int n=s2.size();
+        int ans=0;
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                solve(i,j,s1,s2,dp,ans);
             }
         }
-
         return ans;
     }
 };
