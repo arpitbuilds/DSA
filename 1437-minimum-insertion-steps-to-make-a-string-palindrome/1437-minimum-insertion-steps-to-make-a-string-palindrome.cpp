047335b3e082e31f6c3bc46ecef5d38ef5b59102
Arpit Singh <arpitbuilds@gmail.com>
2025-11-11 10:29:49 +0530

    Time: 37 ms (29.35%), Space: 31.5 MB (37.59%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,38 +1,45 @@
 class Solution {
 public:
-        int longestCommonSubsequence(string s, string t) {
-         int m = s.size();
-        int n = t.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
-        for (int i = 0; i <= m; i++) { 
+    int solve(int x, int y, string s, string t, vector<vector<int>>& dp) {
+        // if (x == 0 || y == 0) {
+        //     return 0;
+        // }
+        // if (dp[x][y] != -1) {
+        //     return dp[x][y];
+        // }
+        // if (s[x - 1] == t[y - 1]) {
+        //     return dp[x][y] = 1 + solve(x - 1, y - 1, s, t, dp);
+        // } else {
+        //     return dp[x][y] = 0 + max(solve(x - 1, y, s, t, dp),
+        //                               solve(x, y - 1, s, t, dp));
+        // }
+        for (int i = 0; i <= x; i++) {
             dp[i][0] = 0;
         }
-        for (int j = 0; j <= n; j++) {
+        for (int j = 0; j <= y; j++) {
             dp[0][j] = 0;
         }
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
+        for (int i = 1; i <= x; i++) {
+            for (int j = 1; j <= y; j++) {
                 if (s[i - 1] == t[j - 1]) {
                     dp[i][j] = 1 + dp[i - 1][j - 1];
-                }
-                else{
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                } else {
+                    dp[i][j] = 0 + max(dp[i - 1][j], dp[i][j - 1]);
                 }
             }
         }
-
-        return dp[m][n];
+        return dp[x][y];
     }
-    int longestPalindromeSubseq(string s) {
-          string t=s;
-          reverse(s.begin(),s.end());
-          return longestCommonSubsequence(s,t);
+    int solvep(string s) {
+        string t = s;
+        int n = s.size();
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, -1));
+        reverse(s.begin(), s.end());
+        return solve(n, n, s, t, dp);
     }
     int minInsertions(string s) {
-        int n=s.size();
-        int pn=longestPalindromeSubseq(s);
-        return n-pn;
-
-        
+        int n = s.size();
+        int pn = solvep(s);
+        return n - pn;
     }
 };
