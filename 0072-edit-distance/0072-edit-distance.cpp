5c8b1aad180ad81b580677bf6fd95916fc055087
Arpit Singh <arpitbuilds@gmail.com>
2025-11-11 11:56:50 +0530

    Time: 11 ms (30.3%), Space: 13.3 MB (30.24%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,27 +1,47 @@
 class Solution {
 public:
-    int solve(int x,int y,string &s,string &w,vector<vector<int>>&dp){
-        if(x<0){
-            return y+1;
+    // int solve(int x, int y, string& s, string& w, vector<vector<int>>& dp) {
+    //     if (x == 0) {
+    //         return y;
+    //     }
+    //     if (y == 0) {
+    //         return x;
+    //     }
+    //     if (dp[x][y] != -1) {
+    //         return dp[x][y];
+    //     }
+    //     if (s[x - 1] == w[y - 1]) {
+    //         return dp[x][y] = 0 + solve(x - 1, y - 1, s, w, dp);
+    //     }
+    //     int inser = 1 + solve(x, y - 1, s, w, dp);
+    //     int del = 1 + solve(x - 1, y, s, w, dp);
+    //     int rep = 1 + solve(x - 1, y - 1, s, w, dp);
+    //     return dp[x][y] = min({inser, del, rep});
+    // }
+    int minDistance(string s, string w) {
+        int m = s.size();
+        int n = w.size();
+        vector<vector<int>> dp(m + 1, vector<int>(n+1, 0));
+        // return solve(n1,n2,w1,w2,dp);
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
         }
-        if(y<0){
-            return x+1;
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
         }
-        if(dp[x][y]!=-1){
-            return dp[x][y];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s[i - 1] == w[j - 1]) {
+                    dp[i][j] = 0 + dp[i - 1][j - 1];
+                }
+                else{
+                int inser = 1 + dp[i][j - 1];
+                int del = 1 + dp[i - 1][j];
+                int rep = 1 + dp[i - 1][j - 1];
+                dp[i][j] = min({inser, del, rep});
+                }
+            }
         }
-        if(s[x]==w[y]){
-            return dp[x][y]=0+solve(x-1,y-1,s,w,dp);
-        }
-        int inser=1+solve(x,y-1,s,w,dp);
-        int del=1+solve(x-1,y,s,w,dp);
-        int rep=1+solve(x-1,y-1,s,w,dp);
-        return dp[x][y]=min({inser,del,rep});
-    }
-    int minDistance(string w1, string w2) {
-        int n1=w1.size();
-        int n2=w2.size();
-        vector<vector<int>>dp(n1,vector<int>(n2,-1));
-        return solve(n1-1,n2-1,w1,w2,dp);
+        return dp[m][n];
     }
 };
