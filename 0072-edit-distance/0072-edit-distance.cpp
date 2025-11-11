2c38e41d0f33d605f39529103854f5a2a4b6ff75
Arpit Singh <arpitbuilds@gmail.com>
2025-11-11 11:33:06 +0530

    Time: 11 ms (30.3%), Space: 14.3 MB (22.95%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
-        int n1=word1.size();
-        int n2=word2.size();
-        vector<vector<int>> dp(n1+1,vector<int>(n2+1,0));
-        for(int i=0;i<n1+1;i++) dp[i][0]=i;
-        for(int j=0;j<n2+1;j++) dp[0][j]=j;
-        for(int i=1;i<n1+1;i++){
-            for(int j=1;j<n2+1;j++){
-                if(word1[i-1]==word2[j-1]){
-                    dp[i][j]=dp[i-1][j-1];
-                }
-                else{
-                    int insertion=1+dp[i][j-1];
-                    int deletion=1+dp[i-1][j];
-                    int replacement=1+dp[i-1][j-1];
-                    dp[i][j]=min(insertion,min(deletion,replacement));
-                }
-            }
+    int solve(int x,int y,string &s,string &w,vector<vector<int>>&dp){
+        if(x<0){
+            return y+1;
         }
-        return dp[n1][n2];
+        if(y<0){
+            return x+1;
+        }
+        if(dp[x][y]!=-1){
+            return dp[x][y];
+        }
+        if(s[x]==w[y]){
+            return dp[x][y]=0+solve(x-1,y-1,s,w,dp);
+        }
+        int inser=1+solve(x,y-1,s,w,dp);
+        int del=1+solve(x-1,y,s,w,dp);
+        int rep=1+solve(x-1,y-1,s,w,dp);
+        return dp[x][y]=min({inser,del,rep});
+    }
+    int minDistance(string w1, string w2) {
+        int n1=w1.size();
+        int n2=w2.size();
+        vector<vector<int>>dp(n1,vector<int>(n2,-1));
+        return solve(n1-1,n2-1,w1,w2,dp);
     }
 };
