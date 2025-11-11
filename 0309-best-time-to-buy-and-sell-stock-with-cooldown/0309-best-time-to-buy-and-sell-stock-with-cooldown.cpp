12cea8d05d2f990f828b7653cf65b05635cb7236
Arpit Singh <arpitbuilds@gmail.com>
2025-11-11 21:13:15 +0530

    Time: 0 ms (100%), Space: 16.3 MB (33.99%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    int solve(int ind,int buy,vector<int>&prices,vector<vector<int>>&dp){
-        if(ind>=prices.size()){
+    int solve(int ind,int b,vector<int>&p,vector<vector<int>>&dp){
+        int n=p.size();
+        if(ind>=n){
             return 0;
         }
-        if(dp[ind][buy]!=-1){
-            return dp[ind][buy];
+        int pf=0;
+        if(dp[ind][b]!=-1){
+            return dp[ind][b];
         }
-        if(buy){
-            dp[ind][buy]=max(-prices[ind]+solve(ind+1,0,prices,dp),0+solve(ind+1,1,prices,dp));
+        if(b){
+            pf=max((-p[ind]+solve(ind+1,0,p,dp)),(0+solve(ind+1,1,p,dp)));
         }
-      else{
-             dp[ind][buy]= max(prices[ind]+solve(ind+2,1,prices,dp),0+solve(ind+1,0,prices,dp));
-      }
-        return dp[ind][buy];
-
+        else{
+            pf=max((p[ind]+solve(ind+2,1,p,dp)),(0+solve(ind+1,0,p,dp)));
+        }
+        return dp[ind][b]=pf;
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
         vector<vector<int>>dp(n,vector<int>(2,-1));
         return solve(0,1,prices,dp);
-        
     }
 };
