c89e60fac0d128033d9236075cbdbe81785e274e
Arpit Singh <arpitbuilds@gmail.com>
2025-11-11 22:22:27 +0530

    Time: 587 ms (8.52%), Space: 296.6 MB (6.31%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,35 +1,24 @@
 class Solution {
 public:
-//    int solve(int ind, int prev, vector<int>& nums, vector<vector<int>>& dp) {
-//         int n = nums.size();
-//         if (ind == n) {
-//             return 0;
-//         }
-//         if (dp[ind][prev + 1] != -1) {
-//             return dp[ind][prev + 1];
-//         }
-//         int ntake = 0 + solve(ind + 1, prev, nums, dp);
-//         int take=0;
-//         if (prev == -1 || nums[ind] > nums[prev]) {
-//          take =  1 + solve(ind + 1, ind, nums, dp);
-//         }x
-//         return dp[ind][prev + 1] = max(take,ntake);
-//     }
-    int lengthOfLIS(vector<int>& arr) {
-        // int n=nums.size();
-        // vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        // return solve(0,-1,nums,dp);
-        int n=arr.size();
-        vector<int>dp(n,1);
-        int maxi=1;
-        for(int i=0;i<n;i++){
-            for(int prev=0;prev<i;prev++){
-                if(arr[prev] <arr[i]){
-                    dp[i]=max(dp[i],1+dp[prev]);
-                }
-            }
-            maxi=max(dp[i],maxi);
+    int solve(int ind,vector<int>&nums,vector<vector<int>>&dp,int prev){
+        int n=nums.size();
+        if(ind==n){
+            return 0;
         }
-        return maxi;
+        if(dp[ind][prev+1]!=-1){
+            return dp[ind][prev+1];
+        }
+        int ntake=0+solve(ind+1,nums,dp,prev);
+        int take=0;
+        if(prev==-1 || nums[ind]>nums[prev]){
+            take=1+solve(ind+1,nums,dp,ind);
+        }
+        return dp[ind][prev+1]=max(take,ntake);
+
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        return solve(0,nums,dp,-1);
     }
 };
