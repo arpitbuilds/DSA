84c334b32b441e8cc8aa9984eedc8aba84a9026f
Arpit Singh <arpitbuilds@gmail.com>
2025-11-12 20:35:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
+++ Difficulty: Medium/Tree Boundary Traversal/tree-boundary-traversal.cpp
@@ -21,65 +21,64 @@ class Solution {
             return true;
         }
         else{
-        return false;
+            return false;
         }
     }
-    void addr(Node*root,vector<int>&ans){
-      Node *cur=root->right;
-        vector<int>temp;
-        while(cur){
-           if(!isleaf(cur)){
-            temp.push_back(cur->data);
-        }
-        if(cur->right){
-            cur=cur->right;
-        }
-        else{
-            cur=cur->left;
-        }
-        }
-        for(int i=temp.size()-1;i>=0;i--){
-            ans.push_back(temp[i]);
-        }
-    }
-      void addl(Node*root,vector<int>&ans){
+    void addl(Node*root,vector<int>&res){
         Node*cur=root->left;
         while(cur){
-             if(!isleaf(cur)){
-             ans.push_back(cur->data);
-        }
+            if(!isleaf(cur)){
+                res.push_back(cur->data);
+            }
             if(cur->left){
                 cur=cur->left;
             }
             else{
                 cur=cur->right;
             }
+            
         }
     }
-    void adde(Node*root,vector<int>&ans){
-        if(root==NULL){
-            return;
+    void addr(Node*root,vector<int>&res){
+           Node*cur=root->right;
+           vector<int>ans;
+        while(cur){
+            if(!isleaf(cur)){
+                ans.push_back(cur->data);
+            }
+            if(cur->right){
+                cur=cur->right;
+            }
+            else{
+                cur=cur->left;
+            }
+            
         }
-        if(isleaf(root)){
-            ans.push_back(root->data);
-            return;
-        }
-        adde(root->left,ans);
-        adde(root->right,ans);
-    }
-    
-    vector<int> boundaryTraversal(Node *root) {
-        // code here
-          vector<int>res;
-        if(!root){
-            return res;
-        }
-         if(!isleaf(root)){
-           res.push_back(root->data); 
+        for(int i=ans.size()-1;i>=0;i--){
+            res.push_back(ans[i]);
         }
         
+    }
+    void addt(Node*root,vector<int>&res){
+        if(root==NULL){
+            return ;
+        }
+        if(isleaf(root)){
+            res.push_back(root->data);
+            // return;
+        }
+        addt(root->left,res);
+        addt(root->right,res);
+    }
+    vector<int> boundaryTraversal(Node *root) {
+        // code here
+        vector<int>res;
+        if(root==NULL){
+            return res;
+        }
+       if (!isleaf(root)) res.push_back(root->data);
         addl(root,res);
-        adde(root,res);
+        addt(root,res);
         addr(root,res);
         return res;
         
