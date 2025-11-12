6770ea48ff7aee812732d3ac4ec38bf0862c8e2c
Arpit Singh <arpitbuilds@gmail.com>
2025-11-12 23:17:38 +0530

    Time: 3 ms (86.38%), Space: 20.9 MB (23.55%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,51 +1,51 @@
 class Solution {
 public:
-     int solve(vector<int>& heights) {
-        int maxi=INT_MIN;
-        int n=heights.size();
-        stack<int>st;
+    int solve(vector<int>&v){
+        int n=v.size();
         vector<int>pse(n);
         vector<int>nse(n);
+        stack<int>st;
         for(int i=0;i<n;i++){
-            while(!st.empty() && heights[st.top()]>=heights[i]){
+            while(!st.empty() && v[st.top()]>=v[i]){
                 st.pop();
             }
             pse[i]=st.empty()?-1:st.top();
             st.push(i);
         }
-        stack<int>st1;
+             stack<int>st2;
         for(int i=n-1;i>=0;i--){
-            while(!st1.empty() && heights[st1.top()]>heights[i]){
-                st1.pop();
+            while(!st2.empty() && v[st2.top()]>v[i]){
+                st2.pop();
             }
-            nse[i]=st1.empty()?n:st1.top();
-            st1.push(i);
+            nse[i]=st2.empty()?n:st2.top();
+            st2.push(i);
         }
-        for(int i=0;i<n;i++){
-            int left=nse[i];
-            int right=pse[i];
-            int width=left-right-1;
-            maxi=max(maxi,width*heights[i]);
-        }
-        return maxi;
+       int maxi=INT_MIN;
+       for(int i=0;i<n;i++){
+        int r=nse[i];
+        int l=pse[i];
+        int w=r-l-1;
+        maxi=max(maxi,w*v[i]);
+       }
+       return maxi;
     }
     int maximalRectangle(vector<vector<char>>& matrix) {
-        int n=matrix.size();
-        int m=matrix[0].size();
+        int m=matrix.size();
+        int n=matrix[0].size();
         int res=0;
-        vector<int>height(m,0);
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-             if(matrix[i][j]=='1'){
-                height[j]+=1;
-             }
-             else{
-                height[j]=0;
-             }
-            
+        vector<int>v(n,0);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]=='1'){
+                    v[j]+=1;
+                }
+                else{
+                 v[j]=0;
+                }
             }
-             res=max(res,solve(height));
+         res=max(res,solve(v));
         }
-        return res;
+   
+    return res;
     }
 };
