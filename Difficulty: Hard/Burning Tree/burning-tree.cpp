37f4c7cf02a2ede158c726298b408989205ce35e
Arpit Singh <arpitbuilds@gmail.com>
2025-11-12 13:57:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Burning Tree/burning-tree.cpp
+++ Difficulty: Hard/Burning Tree/burning-tree.cpp
@@ -16,55 +16,57 @@ class Solution {
   public:
     int minTime(Node* root, int target) {
         // code here
-           Node* start = nullptr;
+        Node*start=NULL;
+         unordered_map<int,Node*> mpp;
         queue<Node*> q;
         q.push(root);
-        unordered_map<int, Node*> mpp;
         while (!q.empty()) {
-            int s = q.size();
-         
-
-            for (int i = 0; i < s; i++) {
-                Node* node = q.front();
-                q.pop();
-                if (node->data == target) start = node;
-                if (node->left) {
-                    q.push(node->left);
-                    mpp[node->left->data] = node;
-                }
-                if (node->right) {
-                    mpp[node->right->data] = node;
-                    q.push(node->right);
-                }
+            Node* node = q.front();
+            q.pop();
+            if(node->data==target){
+                start=node;
+            }
+            if (node->left) {
+                q.push(node->left);
+                mpp[node->left->data] = node;
+            }
+            if (node->right) {
+                q.push(node->right);
+                mpp[node->right->data] = node;
             }
         }
-         unordered_map<int, int> vis;
-        int time=0;
-          q.push(start);
-        vis[start->data] = true;
+        q.push(start);
+        unordered_map<int,int>vis;
+        vis[start->data]=1;
        
-        while (!q.empty()) {
-               bool burned = false;
-            int s = q.size();
-            for (int i = 0; i < s; i++) {
-                Node* node = q.front();
+        int time=0;
+        while(!q.empty()){
+            int s=q.size();
+             bool burned=false;
+            for(int i=0;i<s;i++){
+                Node*node=q.front();
                 q.pop();
-               
-                vis[node->data] = 1;
-                if (node->left && !vis[node->left->data]) {
+                vis[node->data]=1;
+                if(node->left && !vis[node->left->data]){
                     q.push(node->left);
                     burned=true;
+                     vis[node->left->data] = 1;
                 }
-                if (node->right && !vis[node->right->data]) {
+                if(node->right && !vis[node->right->data]){
                     q.push(node->right);
                     burned=true;
+                     vis[node->right->data] = 1;
                 }
-                if (mpp[node->data] && !vis[mpp[node->data]->data]) {
+                if(mpp[node->data]  && !vis[mpp[node->data]->data]){
                     q.push(mpp[node->data]);
                     burned=true;
+                     vis[mpp[node->data]->data] = 1;
                 }
+                
             }
-              if (burned) time++;
+            if(burned){
+                time+=1;
+            }
         }
         return time;
     }
