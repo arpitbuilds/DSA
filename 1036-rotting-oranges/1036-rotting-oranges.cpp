882fa8af9c2efd96c5e94a4a40229679632da555
Arpit Singh <arpitbuilds@gmail.com>
2025-11-17 11:42:52 +0530

    Time: 0 ms (100%), Space: 17 MB (54.45%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,42 +1,41 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<int>>v(m,vector<int>(n,0));
-        int tm=0;
-        queue<pair<pair<int,int>,int>>q;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==2 ){
-                    v[i][j]=2;
-                    q.push({{i,j},0});
-
+        int m = grid.size();
+        int n = grid[0].size();
+        int tm = 0;
+        queue<pair<pair<int, int>, int>> q;
+        vector<vector<int>> v(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 2) {
+                    v[i][j] = 2;
+                    q.push({{i, j}, 0});
                 }
             }
         }
-        int drow[]={-1,0,1,0};
-        int dcol[]={0,1,0,-1};
-        while(!q.empty()){
-            int r=q.front().first.first;
-            int c=q.front().first.second;
-            int t=q.front().second;
+        int delr[] = {-1, 0, 1, 0};
+        int delc[] = {0, 1, 0, -1};
+        while (!q.empty()) {
+            int x = q.front().first.first;
+            int y = q.front().first.second;
+            int t = q.front().second;
             q.pop();
-            for(int i=0;i<4;i++){
-                int nrow=r+drow[i];
-                int ncol=c+dcol[i];
-                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && v[nrow][ncol]!=2 && grid[nrow][ncol]==1){
-                    q.push({{nrow,ncol},t+1});
-                    v[nrow][ncol]=2;
-                    tm=max(tm,t+1);
-
-                }
+           for (int i = 0; i < 4; i++) {
+                    int nrow = x + delr[i];
+                    int ncol = y + delc[i];
+                    if (nrow >= 0 && ncol >= 0 && nrow < m && ncol < n &&
+                        v[nrow][ncol] != 2 && grid[nrow][ncol] == 1) {
+                        v[nrow][ncol] = 2;
+                        q.push({{nrow, ncol}, t + 1});
+                        tm = max(tm, t + 1);
+                    }
+                
             }
-
         }
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(v[i][j]!=2 && grid[i][j]==1){
+                if(grid[i][j]==1 && v[i][j]!=2){
                     return -1;
                 }
             }
