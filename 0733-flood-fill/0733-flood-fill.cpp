e79d1d4b87ea8c679b78efe7f18e6fa75c6e2c60
Arpit Singh <arpitbuilds@gmail.com>
2025-11-17 16:10:29 +0530

    Time: 0 ms (100%), Space: 17.9 MB (89.06%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
-    void dfs(int inic,int sr,int sc,vector<vector<int>>& img,vector<vector<int>>& ans,int color){
-        ans[sr][sc]=color;
-        int r=sr;
-        int c=sc;
-        int m=img.size();
-        int n=img[0].size();
-          int drow[]={-1,0,1,0};
-      int dcol[]={0,1,0,-1};
-        for(int i=0;i<4;i++){
-            int nrow=r+drow[i];
-            int ncol=c+dcol[i];
-            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && img[nrow][ncol]==inic && ans[nrow][ncol]!=color){
-                dfs(inic,nrow,ncol,img,ans,color);
+    void solve(int m, int n, int sr, int sc, int inic, int color,
+               vector<vector<int>>& ans, vector<vector<int>>& img) {
+        ans[sr][sc] = color;
+        int delr[] = {-1, 0, +1, -0};
+        int delc[] = {0, 1, 0, -1};
+        for (int i = 0; i < 4; i++) {
+            int nrow = sr + delr[i];
+            int ncol = sc + delc[i];
 
+            if (nrow >= 0 && nrow < m && ncol < n && ncol >= 0 &&
+                img[nrow][ncol] == inic && ans[nrow][ncol] != color) {
+                solve(m, n, nrow, ncol, inic, color, ans, img);
             }
         }
     }
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-        vector<vector<int>>ans=image;
-        int inic=image[sr][sc];
-        dfs(inic,sr,sc,image,ans,color);
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
+                                  int color) {
+        int m = image.size();
+        int n = image[0].size();
+        vector<vector<int>> ans = image;
+        int inic = image[sr][sc];
+        solve(m, n, sr, sc, inic, color, ans, image);
         return ans;
     }
 };
