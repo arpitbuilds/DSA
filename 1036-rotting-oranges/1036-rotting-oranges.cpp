a9c17543b3a04ba4117d5535a4306cad642f4ea9
Arpit Singh <arpitbuilds@gmail.com>
2025-11-18 10:56:50 +0530

    Time: 3 ms (29.59%), Space: 17.1 MB (46.31%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -4,8 +4,8 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         int tm = 0;
-        queue<pair<pair<int, int>, int>> q;
         vector<vector<int>> v(m, vector<int>(n, 0));
+        queue<pair<pair<int, int>, int>> q;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 2) {
@@ -17,25 +17,25 @@ public:
         int delr[] = {-1, 0, 1, 0};
         int delc[] = {0, 1, 0, -1};
         while (!q.empty()) {
-            int x = q.front().first.first;
-            int y = q.front().first.second;
+            int r = q.front().first.first;
+            int c = q.front().first.second;
             int t = q.front().second;
             q.pop();
-           for (int i = 0; i < 4; i++) {
-                    int nrow = x + delr[i];
-                    int ncol = y + delc[i];
-                    if (nrow >= 0 && ncol >= 0 && nrow < m && ncol < n &&
-                        v[nrow][ncol] != 2 && grid[nrow][ncol] == 1) {
-                        v[nrow][ncol] = 2;
-                        q.push({{nrow, ncol}, t + 1});
-                        tm = max(tm, t + 1);
-                    }
-                
+
+            for (int i = 0; i < 4; i++) {
+                int nrow = r + delr[i];
+                int ncol = c + delc[i];
+                if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n &&
+                    grid[nrow][ncol] == 1 && v[nrow][ncol] != 2) {
+                    q.push({{nrow, ncol}, t + 1});
+                    v[nrow][ncol] = 2;
+                    tm = max(tm, t + 1);
+                }
             }
         }
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==1 && v[i][j]!=2){
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1 && v[i][j] != 2) {
                     return -1;
                 }
             }
