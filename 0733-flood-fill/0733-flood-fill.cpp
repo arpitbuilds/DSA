517a343ee62f8ec30d43ce0d1ec3464ae4de2b76
Arpit Singh <arpitbuilds@gmail.com>
2025-11-18 11:14:09 +0530

    Time: 0 ms (100%), Space: 17.9 MB (89%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
-    void solve(int m, int n, int sr, int sc, int inic, int color,
-               vector<vector<int>>& ans, vector<vector<int>>& img) {
+    void solve(vector<vector<int>>& img, vector<vector<int>>& ans, int sr,
+               int sc, int color, int inic) {
+                int m=img.size();
+                int n=img[0].size();
         ans[sr][sc] = color;
-        int delr[] = {-1, 0, +1, -0};
+        int delr[] = {-1, 0, 1, 0};
         int delc[] = {0, 1, 0, -1};
         for (int i = 0; i < 4; i++) {
             int nrow = sr + delr[i];
             int ncol = sc + delc[i];
-
-            if (nrow >= 0 && nrow < m && ncol < n && ncol >= 0 &&
+            if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n &&
                 img[nrow][ncol] == inic && ans[nrow][ncol] != color) {
-                solve(m, n, nrow, ncol, inic, color, ans, img);
+                solve(img, ans, nrow, ncol, color, inic);
             }
         }
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
                                   int color) {
-        int m = image.size();
-        int n = image[0].size();
         vector<vector<int>> ans = image;
         int inic = image[sr][sc];
-        solve(m, n, sr, sc, inic, color, ans, image);
+        solve(image, ans, sr, sc, color, inic);
         return ans;
     }
 };
