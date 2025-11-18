ad4467ffc9c11be93d566541aff01e39f5b09738
Arpit Singh <arpitbuilds@gmail.com>
2025-11-18 11:44:14 +0530

    Time: 7 ms (77.28%), Space: 31.6 MB (85.3%) - LeetHub

--- 1073-number-of-enclaves/1073-number-of-enclaves.cpp
+++ 1073-number-of-enclaves/1073-number-of-enclaves.cpp
@@ -9,23 +9,23 @@ public:
             for (int j = 0; j < n; j++) {
                 if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
                     if (grid[i][j] == 1) {
-                        vis[i][j] = 1;
                         q.push({i, j});
+                        vis[i][j] = 1;
                     }
                 }
             }
         }
-        int delr[] = {-1, 0, 1, 0};
-        int delc[] = {0, 1, 0, -1};
         while (!q.empty()) {
             int r = q.front().first;
             int c = q.front().second;
             q.pop();
-            for (int i = 0; i < 4; i++) {
+            int delr[] = {-1, 0, 1, 0};
+            int delc[] = {0, 1, 0, -1};
+            for (int i = 0; i< 4; i++) {
                 int nrow = r + delr[i];
                 int ncol = c + delc[i];
-                if (nrow >= 0 && nrow < m && ncol < n && ncol >= 0 &&
-                    grid[nrow][ncol] == 1 && vis[nrow][ncol] == 0) {
+                if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n &&
+                    grid[nrow][ncol] == 1 && vis[nrow][ncol] != 1) {
                     q.push({nrow, ncol});
                     vis[nrow][ncol] = 1;
                 }
@@ -34,7 +34,7 @@ public:
         int cnt = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 1 && vis[i][j] == 0) {
+                if (grid[i][j] == 1 && vis[i][j] != 1) {
                     cnt++;
                 }
             }
