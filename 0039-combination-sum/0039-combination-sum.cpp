fb07ab7deda90dfd460da73099961752be3fa6bf
Arpit Singh <arpitbuilds@gmail.com>
2025-11-19 22:17:39 +0530

    Time: 0 ms (100%), Space: 14 MB (90.58%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    void solve(int ind,vector<int>&given,int target,vector<int>&v,set<vector<int>>&res){
-        if(ind==given.size()){
-            if(target==0){
-            res.insert(v);
+    void solve(int ind,vector<int>&v,vector<vector<int>>&ans,int t,vector<int>&c){
+        if(ind==c.size()){
+            if(t==0){
+                ans.push_back(v);
+                
             }
-            return;
+            return ;
         }
-        if(given[ind]<=target){
-            v.push_back(given[ind]);
-            solve(ind,given,target-given[ind],v,res);
+        if(c[ind]<=t){
+            v.push_back(c[ind]);
+            solve(ind,v,ans,t-c[ind],c);
             v.pop_back();
         }
-        solve(ind+1,given,target,v,res);
-    }
+        solve(ind+1,v,ans,t,c);
+    } 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         vector<int>v;
-        set<vector<int>>res;
-        solve(0,candidates,target,v,res);
-        vector<vector<int>>ans(res.begin(),res.end());
+        vector<vector<int>>ans;
+        solve(0,v,ans,target,candidates);
         return ans;
-        
     }
 };
