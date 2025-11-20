0c82e8220360a18feb57fedc1c78feaad5345d16
Arpit Singh <arpitbuilds@gmail.com>
2025-11-20 11:42:40 +0530

    Time: 50 ms (5.25%), Space: 54.6 MB (5.89%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,52 +1,50 @@
 class Solution {
 public:
-     bool can(int r , int c ,vector<string>&b,int n){
+    bool check(int r,int c,int n,vector<string>v){
         int tr=r;
         int tc=c;
-        while(r>=0 && c>=0){
-            if(b[r--][c--]=='Q'){
-                return false;
-            }
-        }
-        r=tr;
-        c=tc;
         while(c>=0){
-            if(b[r][c--]=='Q'){
+            if(v[r][c--]=='Q'){
                 return false;
             }
         }
         r=tr;
         c=tc;
         while(r<n && c>=0){
-            if(b[r++][c--]=='Q'){
+            if(v[r++][c--]=='Q'){
+                return false;
+            }
+        }
+        r=tr;
+        c=tc;
+        while(r>=0 && c>=0){
+            if(v[r--][c--]=='Q'){
                 return false;
             }
         }
         return true;
-     }
-     void solve(int c, vector<vector<string>>&res,vector<string>&b,int n ){
+    }
+    void solve(int c,vector<vector<string>>&ans,vector<string>v,int n){
         if(c==n){
-            res.push_back(b);
-            return;
+            ans.push_back(v);
+            return ;
         }
         for(int r=0;r<n;r++){
-            if(can(r,c,b,n)){
-                b[r][c]='Q';
-                solve(c+1,res,b,n);
-                b[r][c]='.';
+            if(check(r,c,n,v)){
+                v[r][c]='Q';
+                solve(c+1,ans,v,n);
+                v[r][c]='.';
             }
         }
-
-     }
+    }
     vector<vector<string>> solveNQueens(int n) {
-         vector<vector<string>> ans;
-         vector<string>b(n);
-         string s(n,'.');
-         for(int i=0;i<n;i++){
-            b[i]=s;
-         }
-         solve(0,ans,b,n);
-         return ans;
-        
+        vector<vector<string>>ans;
+        vector<string>v(n);
+        string s(n,'.');
+        for(int i=0;i<n;i++){
+            v[i]=s;
+        }
+        solve(0,ans,v,n);
+        return ans;
     }
 };
