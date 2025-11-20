c849257f5f282ad9539f416369d8eb2d5b01fe61
Arpit Singh <arpitbuilds@gmail.com>
2025-11-20 12:20:45 +0530

    Time: 187 ms (90.48%), Space: 10.4 MB (84.69%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    bool dfs(vector<vector<char>>& board,string &word,int i,int j,int idx,int m,int n){
-        if(idx==word.size()){
+    bool dfs(vector<vector<char>>& board, string& word, int i, int j, int idx,
+             int m, int n) {
+        if (idx == word.size()) {
             return true;
         }
-        if(i<0 || i>=m || j<0 || j>=n || board[i][j]!=word[idx]){
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != word[idx]) {
             return false;
         }
-        char temp=board[i][j];
-        board[i][j]='$';
-            bool found = dfs(board, word, i+1, j, idx+1,m,n) ||
-                     dfs(board, word, i-1, j, idx+1,m,n) ||
-                     dfs(board, word, i, j+1, idx+1,m,n) ||
-                     dfs(board, word, i, j-1, idx+1,m,n);
+        char temp = board[i][j];
+        board[i][j] = '$';
+        bool op1 = dfs(board, word, i + 1, j, idx + 1, m, n);
+        bool op2 = dfs(board, word, i - 1, j, idx + 1, m, n);
+        bool op3 = dfs(board, word, i, j + 1, idx + 1, m, n);
+        bool op4 = dfs(board, word, i, j - 1, idx + 1, m, n);
 
-
-        board[i][j]=temp;
-        return found;
+        board[i][j] = temp;
+        return op1 || op2 || op3 || op4;
     }
     bool exist(vector<vector<char>>& board, string word) {
-        int m=board.size();
-        int n=board[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j]==word[0]){
-                    if(dfs(board,word,i,j,0,m,n)){
+        int m = board.size();
+        int n = board[0].size();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word[0]) {
+                    if (dfs(board, word, i, j, 0, m, n)) {
                         return true;
                     }
                 }
