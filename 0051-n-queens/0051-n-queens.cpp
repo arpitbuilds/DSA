96c3494d87a620506e46ec5f3778cd0a5beab771
Arpit Singh <arpitbuilds@gmail.com>
2025-11-20 14:35:57 +0530

    Time: 3 ms (64.1%), Space: 10.1 MB (74.17%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,8 +1,15 @@
 class Solution {
 public:
-    bool check(int r,int c,int n,vector<string>v){
+    bool check(int r,int c,vector<vector<string>>&ans,vector<string>&v,int n){
         int tr=r;
         int tc=c;
+        while(r>=0 && c>=0){
+            if(v[r--][c--]=='Q'){
+                return false;
+            }
+        }
+        r=tr;
+        c=tc;
         while(c>=0){
             if(v[r][c--]=='Q'){
                 return false;
@@ -12,25 +19,18 @@ public:
         c=tc;
         while(r<n && c>=0){
             if(v[r++][c--]=='Q'){
-                return false;
-            }
-        }
-        r=tr;
-        c=tc;
-        while(r>=0 && c>=0){
-            if(v[r--][c--]=='Q'){
-                return false;
+            return false;
             }
         }
         return true;
     }
-    void solve(int c,vector<vector<string>>&ans,vector<string>v,int n){
+    void solve(int c,vector<vector<string>>&ans,vector<string>&v,int n){
         if(c==n){
             ans.push_back(v);
             return ;
         }
         for(int r=0;r<n;r++){
-            if(check(r,c,n,v)){
+            if(check(r,c,ans,v,n)){
                 v[r][c]='Q';
                 solve(c+1,ans,v,n);
                 v[r][c]='.';
@@ -39,12 +39,13 @@ public:
     }
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>>ans;
-        vector<string>v(n);
         string s(n,'.');
+        vector<string>v(n);
         for(int i=0;i<n;i++){
-            v[i]=s;
+          v[i]=s;
         }
         solve(0,ans,v,n);
         return ans;
+
     }
 };
