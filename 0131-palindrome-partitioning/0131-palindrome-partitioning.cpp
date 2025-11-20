461da8b633762e9b446962d3a040c340e2668bd0
Arpit Singh <arpitbuilds@gmail.com>
2025-11-20 11:00:41 +0530

    Time: 66 ms (22.21%), Space: 61.7 MB (39.94%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    bool check(string s,int l,int r){
+    bool check(int l,int r,string s){
         while(l<=r){
-            if(s[l]!=s[r]){
-                return false;
-            }
-            l++;
-            r--;
+        if(s[l]!=s[r]){
+            return false;
+        }
+        l++;
+        r--;
         }
         return true;
     }
-    void fun(int ind,string s,vector<string>&path, vector<vector<string>>&ans){
+    void solve(int ind,vector<string>&v,vector<vector<string>>&ans,string s){
         if(ind==s.size()){
-            ans.push_back(path);
-            return ;
+            ans.push_back(v);
+            return;
         }
         for(int i=ind;i<s.size();i++){
-            if(check(s,ind,i)==true){
-                path.push_back(s.substr(ind,i-ind+1));
-                fun(i+1,s,path,ans);
-                path.pop_back();
+            if(check(ind,i,s)){
+                v.push_back(s.substr(ind,i-ind+1));
+                solve(i+1,v,ans,s);
+                v.pop_back();
             }
         }
     }
     vector<vector<string>> partition(string s) {
         vector<vector<string>>ans;
-        vector<string>path;
-        fun(0,s,path,ans);
+        vector<string>v;
+        solve(0,v,ans,s);
         return ans;
     }
-    
 };
