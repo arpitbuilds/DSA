f7bf7f4aa4d7eecd75bfe05e45549829ab33844b
Arpit Singh <arpitbuilds@gmail.com>
2025-11-20 14:13:23 +0530

    Time: 781 ms (33.52%), Space: 11.5 MB (44.66%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,32 +1,32 @@
 class Solution {
-public:
-   bool dfs(vector<vector<char>>&b,string w,int i,int j, int ind,int m,int n){
-    if(ind==w.size()){
-        return true;
-    }
-    if(i<0 || i>=m ||  j>=n ||  j<0 || w[ind]!=b[i][j]){
-        return false;
-    }
-    char temp=b[i][j];
-    b[i][j]='$';
-    bool op1=dfs(b,w,i-1,j,ind+1,m,n);
-    bool op2=dfs(b,w,i,j+1,ind+1,m,n);
-    bool op3=dfs(b,w,i+1,j,ind+1,m,n);
-    bool op4=dfs(b,w,i,j-1,ind+1,m,n);
-    b[i][j]=temp;
-    return op1||op2||op3||op4;
+public: 
+    bool dfs(int i,int j,int ind,vector<vector<char>>& b,string word,int m,int n){
+        if(ind==word.size()){
+            return true;
+        }
+        if(i<0 || i>=m || j>=n || j<0 || word[ind]!=b[i][j]){
+            return false;
+        }
+        char temp=b[i][j];
+        b[i][j]='$';
+        bool op1=dfs(i-1,j,ind+1,b,word,m,n);
+        bool op2=dfs(i,j+1,ind+1,b,word,m,n);
+        bool op3=dfs(i+1,j,ind+1,b,word,m,n);
+        bool op4=dfs(i,j-1,ind+1,b,word,m,n);
+        b[i][j]=temp;
 
+        return op1||op2||op3||op4;
 
-   }
+    }
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
-        int n = board[0].size();
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == word[0]) {
-                    if (dfs(board, word, i, j, 0, m,  n)) {
-                        return true;
-                    }
+        int m=board.size();
+        int n=board[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]==word[0]){
+                   if(dfs(i,j,0,board,word,m,n)){
+                    return true;
+                   }
                 }
             }
         }
