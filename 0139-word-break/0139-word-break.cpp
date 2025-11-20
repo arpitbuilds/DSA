0c38feaa5c778ca2f8829793f7cf8c95b13735ef
Arpit Singh <arpitbuilds@gmail.com>
2025-11-20 15:26:44 +0530

    Time: 55 ms (5%), Space: 33.7 MB (5.5%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    bool solve(int ind,string s,set<string>st,vector<int>&dp){
+    bool solve(int ind,string s, set<string>st,vector<int>&dp){
         if(ind==s.size()){
             return true;
         }
         if(st.find(s)!=st.end()){
             return true;
         }
-        if(dp[ind]!=-1){
+         if(dp[ind]!=-1){
             return dp[ind];
         }
         for(int i=1;i<=s.size();i++){
@@ -18,13 +18,14 @@ public:
         }
         return dp[ind]=false;
     }
-    bool wordBreak(string s, vector<string>& wordDict) {
-        int n=s.length();
+    bool wordBreak(string s, vector<string>& word) {
         set<string>st;
-        for(string w :wordDict){
+        int n=s.size();
+        for(auto w : word){
             st.insert(w);
         }
         vector<int>dp(n,-1);
         return solve(0,s,st,dp);
+                    
     }
 };
