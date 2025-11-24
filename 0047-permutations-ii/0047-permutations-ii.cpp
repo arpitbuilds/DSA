8a456c1f7f594f51fd7ca53a900dc38bd18e15d3
Arpit Singh <arpitbuilds@gmail.com>
2025-11-24 14:06:19 +0530

    Time: 1 ms (75.83%), Space: 11.5 MB (70.92%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,31 +1,31 @@
 class Solution {
 public:
-    void solve(vector<int>& nums, vector<int>& ds, vector<vector<int>>& ans, vector<int>& freq) {
-        if(ds.size() == nums.size()){
+    void solve(vector<int>&nums,vector<int>&ds,vector<vector<int>>&ans,vector<int>&freq){
+        if(nums.size()==ds.size()){
             ans.push_back(ds);
-            return;
+            return ;
         }
-        for(int i = 0; i < nums.size(); i++){
-            if(!freq[i]){
-                // Skip duplicates: if current num == previous num and previous wasn't used
-                if(i > 0 && nums[i] == nums[i-1] && !freq[i-1]) continue;
-
+        for(int i=0;i<nums.size();i++){
+            if(freq[i]==0){
+                if(i>0 && nums[i]==nums[i-1] && !freq[i-1]){
+                    continue;
+                }
                 ds.push_back(nums[i]);
-                freq[i] = 1;
-                solve(nums, ds, ans, freq);
-                freq[i] = 0;
+                freq[i]=1;
+                solve(nums,ds,ans,freq);
+                freq[i]=0;
                 ds.pop_back();
             }
         }
-    }
 
+    }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> ds;
-        vector<int> freq(nums.size(), 0);
-        
-        sort(nums.begin(), nums.end()); // ✅ sort first
-        solve(nums, ds, ans, freq);
+        int n=nums.size();
+        vector<int>ds;
+        vector<vector<int>>ans;
+        vector<int>freq(n,0);
+        sort(nums.begin(),nums.end());
+        solve(nums,ds,ans,freq);
         return ans;
     }
 };
