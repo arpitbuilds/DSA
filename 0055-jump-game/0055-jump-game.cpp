f85532313902330e4a05db9de93eb3b12d6d5931
Arpit Singh <arpitbuilds@gmail.com>
2025-11-26 14:13:32 +0530

    Time: 47 ms (18.08%), Space: 53.1 MB (16.19%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,22 +1,28 @@
 class Solution {
 public:
-    bool solve(vector<int>&nums,int n,int ind,vector<int>&dp){
-        if(ind==n-1){
-            return true;
-        }
-        if(dp[ind]!=-1){
-            return dp[ind];
-        }
-        for(int i=1;i<=nums[ind];i++){
-            if(solve(nums,n,ind+i,dp)==true){
-                return dp[ind]=true;
-            }
-        }
-        return dp[ind]=false;
-    }
+    // bool solve(vector<int>&nums,vector<int>&dp,int n,int ind){
+    //     if(ind==n-1){
+    //         return true;
+    //     }
+    //  for(int i=1;i<=nums[ind];i++){
+    //     if(solve(nums,dp,n,ind+i)){
+    //         return true;
+    //     }
+    //  }
+    //  return false;
+    // }
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        vector<int>dp(n,-1);
-        return solve(nums,n,0,dp);
+        vector<bool>dp(n,false);
+        dp[0]=true;
+        for(int i=1;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(dp[j]==true && j+nums[j]>=i){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[n-1];
     }
 };
