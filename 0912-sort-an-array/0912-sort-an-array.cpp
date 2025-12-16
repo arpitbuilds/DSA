7263f750a2990dfbb514d6f6bab795673ad4503b
Arpit Singh <arpitbuilds@gmail.com>
2025-12-16 11:42:48 +0530

    Time: 627 ms (23.3%), Space: 295.7 MB (7.58%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,45 +1,44 @@
 class Solution {
 public:
-      void merge(vector<int>&arr,int low,int mid,int high){
-      vector<int>temp;
-      int i=low;
-      int j=mid+1;
-      while(i<=mid && j<=high){
-          if(arr[i]>=arr[j]){
-              temp.push_back(arr[j]);
-              j++;
-          }
-          else{
-              temp.push_back(arr[i]);
-              i++;
-          }
-      }
-          while(i<=mid){
-              temp.push_back(arr[i]);
-              i++;
-          }
-          while(j<=high){
-              temp.push_back(arr[j]);
-              j++;
-          }
-         for(int k=low;k<=high;k++){
-          arr[k]=temp[k-low];
-      }
-      }
-   
-  
-    void ms(vector<int>&arr,int low,int high){
-        if(low>=high){
+    void merge(int l,int mid,int high, vector<int>&arr){
+        vector<int>temp;
+        int i=l;
+        int j=mid+1;
+        while(i<=mid && j<=high){
+            if(arr[i]>=arr[j]){
+                temp.push_back(arr[j]);
+                j++;
+            }
+            else{
+                temp.push_back(arr[i]);
+                i++;
+            }
+        }
+        while(i<=mid){
+            temp.push_back(arr[i]);
+            i++;
+        }
+        while(j<=high){
+            temp.push_back(arr[j]);
+            j++;
+        }
+        for(int k=l;k<=high;k++){
+            arr[k]=temp[k-l];
+        }
+    }
+    void ms(int l,int h,vector<int>&nums){
+        if(l>=h){
             return;
         }
-        int mid=(low+high)/2;
-        ms(arr,low,mid);
-        ms(arr,mid+1,high);
-        merge(arr,low,mid,high);
+        int mid=l+(h-l)/2;
+        ms(l,mid,nums);
+        ms(mid+1,h,nums);
+        merge(l,mid,h,nums);
     }
     vector<int> sortArray(vector<int>& nums) {
-        ms(nums,0,nums.size()-1);
-        return nums;
-        
+     if (nums.empty()) return nums;
+       int n=nums.size();
+       ms(0,n-1,nums);
+       return nums;
     }
 };
