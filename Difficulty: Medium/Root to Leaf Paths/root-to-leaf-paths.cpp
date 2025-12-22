c926e30ebff470fe5478ee04e354bc6d5e89bec7
Arpit Singh <arpitbuilds@gmail.com>
2025-12-23 03:53:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Root to Leaf Paths/root-to-leaf-paths.cpp
+++ Difficulty: Medium/Root to Leaf Paths/root-to-leaf-paths.cpp
@@ -16,18 +16,18 @@ struct Node
 
 class Solution {
   public:
-   void solve(vector<vector<int>>&ans,vector<int>&temp,Node*root){
-       if(root==NULL){
-           return ;
-       }
-       temp.push_back(root->data);
-       if(root->left==NULL && root->right==NULL){
-           ans.push_back(temp);
-       }
-       solve(ans,temp,root->left);
-       solve(ans,temp,root->right);
-       temp.pop_back();
-   }
+    void solve(vector<vector<int>>&ans,vector<int>&temp,Node*root){
+        if(root==NULL){
+            return;
+        }
+        temp.push_back(root->data);
+        if(root->left==NULL && root->right==NULL){
+            ans.push_back(temp);
+        }
+        solve(ans,temp,root->left);
+        solve(ans,temp,root->right);
+        temp.pop_back();
+    }
     vector<vector<int>> Paths(Node* root) {
         // code here
         vector<vector<int>>ans;
