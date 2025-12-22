11c257403dcd1aca0b9bfb6a9d054c8aec7f376d
Arpit Singh <arpitbuilds@gmail.com>
2025-12-23 04:44:09 +0530

    Time: 4 ms (66.4%), Space: 15.3 MB (81.51%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
+        unordered_map<int, TreeNode*> mpp;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int s = q.size();
+            for (int i = 0; i < s; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                if (node->left) {
+                    q.push(node->left);
+                    mpp[node->left->val] = node;
+                }
+                if (node->right) {
+                    q.push(node->right);
+                    mpp[node->right->val] = node;
+                }
+            }
+        }
+        unordered_map<int, int> vis;
+        q.push(target);
+        while (k-- && !q.empty()) {
+            int s = q.size();
+            for (int i = 0; i < s; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+                vis[node->val] = 1;
+                if (node->left && !vis[node->left->val]) {
+                    q.push(node->left);
+                }
+                if (node->right && !vis[node->right->val]) {
+                    q.push(node->right);
+                }
+                if (mpp[node->val] && !vis[mpp[node->val]->val]) {
+                    q.push(mpp[node->val]);
+                }
+            }
+        }
+        vector<int> ans;
+        while (!q.empty()) {
+            TreeNode* it = q.front();
+            ans.push_back(it->val);
+            q.pop();
+        }
+        return ans;
+    }
+};
