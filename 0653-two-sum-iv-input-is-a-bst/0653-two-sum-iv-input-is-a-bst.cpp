349e03a2792ee33fa5d9137423835b2179c91295
Arpit Singh <arpitbuilds@gmail.com>
2025-12-23 12:47:34 +0530

    Time: 0 ms (100%), Space: 37 MB (93.53%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -6,33 +6,32 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void solve(TreeNode*root,vector<int>&s){
-        if(root==NULL){
+    void solve(TreeNode* root, vector<int>& s) {
+        if (root == NULL) {
             return;
         }
-        solve(root->left,s);
+        solve(root->left, s);
         s.push_back(root->val);
-        solve(root->right,s);
+        solve(root->right, s);
     }
     bool findTarget(TreeNode* root, int k) {
-        vector<int>s;
-        solve(root,s);
-        int l=0;
-        int e=s.size()-1;
-        while(l<e){
-            int sum=s[l]+s[e];
-            if(sum==k){
+        vector<int> v;
+        solve(root, v);
+        int s = 0;
+        int e = v.size() - 1;
+        while (s < e) {
+           int sum = v[s] + v[e];
+            if (sum == k) {
                 return true;
-            }
-            if(sum<k){
-                l++;
-            }
-            else{
+            }  else if (sum < k) {
+                s++;
+            } else {
                 e--;
             }
         }
