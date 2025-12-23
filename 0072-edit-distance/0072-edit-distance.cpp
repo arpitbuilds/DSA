a9810aa5a396052d499c999ff58cbb80d93ea792
Arpit Singh <arpitbuilds@gmail.com>
2025-12-24 02:12:03 +0530

    Time: 67 ms (8.87%), Space: 83.2 MB (6.79%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,47 +1,27 @@
 class Solution {
 public:
-    // int solve(int x, int y, string& s, string& w, vector<vector<int>>& dp) {
-    //     if (x == 0) {
-    //         return y;
-    //     }
-    //     if (y == 0) {
-    //         return x;
-    //     }
-    //     if (dp[x][y] != -1) {
-    //         return dp[x][y];
-    //     }
-    //     if (s[x - 1] == w[y - 1]) {
-    //         return dp[x][y] = 0 + solve(x - 1, y - 1, s, w, dp);
-    //     }
-    //     int inser = 1 + solve(x, y - 1, s, w, dp);
-    //     int del = 1 + solve(x - 1, y, s, w, dp);
-    //     int rep = 1 + solve(x - 1, y - 1, s, w, dp);
-    //     return dp[x][y] = min({inser, del, rep});
-    // }
-    int minDistance(string s, string w) {
-        int m = s.size();
-        int n = w.size();
-        vector<vector<int>> dp(m + 1, vector<int>(n+1, 0));
-        // return solve(n1,n2,w1,w2,dp);
-        for (int i = 0; i <= m; i++) {
-            dp[i][0] = i;
+    int solve(string w ,string t , int i,int j,vector<vector<int>>&dp){
+        if(i<0){
+        return j+1;
         }
-        for (int j = 0; j <= n; j++) {
-            dp[0][j] = j;
+        if(j<0){
+            return i+1;
         }
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (s[i - 1] == w[j - 1]) {
-                    dp[i][j] = 0 + dp[i - 1][j - 1];
-                }
-                else{
-                int inser = 1 + dp[i][j - 1];
-                int del = 1 + dp[i - 1][j];
-                int rep = 1 + dp[i - 1][j - 1];
-                dp[i][j] = min({inser, del, rep});
-                }
-            }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-        return dp[m][n];
+        if(w[i]==t[j]){
+            return dp[i][j]=0+solve(w,t,i-1,j-1,dp);
+        }
+        int op1=1+solve(w,t,i,j-1,dp);
+        int op2=1+solve(w,t,i-1,j,dp);
+        int op3=1+solve(w,t,i-1,j-1,dp);
+        return dp[i][j]=min({op1,op2,op3});
+    }
+    int minDistance(string w, string t) {
+        int m=w.size();
+        int n=t.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return solve(w,t,m,n,dp);
     }
 };
