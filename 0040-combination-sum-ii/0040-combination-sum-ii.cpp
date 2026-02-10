717d488f93f34a3199101a5438b95e35dac8c6e4
Arpit Singh <arpitbuilds@gmail.com>
2026-02-10 12:25:43 +0530

    Time: 0 ms (100%), Space: 13.9 MB (82.8%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.cpp
+++ 0040-combination-sum-ii/0040-combination-sum-ii.cpp
@@ -1,28 +1,37 @@
 class Solution {
 public:
-    void solve(int ind,vector<int>&v,vector<vector<int>>&ans,int t,vector<int>&c){
-        if(t==0){
-            ans.push_back(v);
-            return ;
+    void solve(int ind, vector<int>& nums, int target,
+               vector<int>& v, vector<vector<int>>& ans) {
+
+        // BASE CONDITION (same style as comb sum 1)
+        if (ind == nums.size()) {
+            if (target == 0) {
+                ans.push_back(v);
+            }
+            return;
         }
-        for(int i=ind;i<c.size();i++){
-            if(i>ind && c[i]==c[i-1]){
-                continue;
-            }
-            if(c[i]>t){
-                break;
-            }
-            v.push_back(c[i]);
-            solve(i+1,v,ans,t-c[i],c);
+
+        // TAKE (only once → ind+1)
+        if (nums[ind] <= target) {
+            v.push_back(nums[ind]);
+            solve(ind + 1, nums, target - nums[ind], v, ans);
             v.pop_back();
         }
 
+        // SKIP duplicates
+        while (ind + 1 < nums.size() && nums[ind] == nums[ind + 1]) {
+            ind++;
+        }
+
+        // NOT TAKE
+        solve(ind + 1, nums, target, v, ans);
     }
+
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>>ans;
-        vector<int>v;
-        sort(candidates.begin(),candidates.end());
-        solve(0,v,ans,target,candidates);
+        sort(candidates.begin(), candidates.end());   // 🔴 REQUIRED
+        vector<vector<int>> ans;
+        vector<int> v;
+        solve(0, candidates, target, v, ans);
         return ans;
     }
 };
