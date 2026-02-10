4154624da67248adf8746ec482acd7fe5dab4bfc
Arpit Singh <arpitbuilds@gmail.com>
2026-02-10 23:03:33 +0530

    Time: 0 ms (100%), Space: 11.4 MB (81.93%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,31 +1,38 @@
 class Solution {
 public:
-    void solve(vector<int>&nums,vector<int>&ds,vector<vector<int>>&ans,vector<int>&freq){
-        if(nums.size()==ds.size()){
+    void solve(vector<int>& nums, vector<int>& ds,
+               vector<vector<int>>& ans, vector<int>& freq) {
+
+        if (ds.size() == nums.size()) {
             ans.push_back(ds);
-            return ;
-        }
-        for(int i=0;i<nums.size();i++){
-            if(freq[i]==0){
-                if(i>0 && nums[i]==nums[i-1] && !freq[i-1]){
-                    continue;
-                }
-                ds.push_back(nums[i]);
-                freq[i]=1;
-                solve(nums,ds,ans,freq);
-                freq[i]=0;
-                ds.pop_back();
-            }
+            return;
         }
 
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (freq[i] == 0) {
+
+                // TAKE
+                ds.push_back(nums[i]);
+                freq[i] = 1;
+                solve(nums, ds, ans, freq);
+                freq[i] = 0;
+                ds.pop_back();
+
+                // 🔥 DUPLICATE SKIP (exactly like subsets / comb sum 2)
+                while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
+                    i++;
+                }
+            }
+        }
     }
+
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        int n=nums.size();
-        vector<int>ds;
-        vector<vector<int>>ans;
-        vector<int>freq(n,0);
-        sort(nums.begin(),nums.end());
-        solve(nums,ds,ans,freq);
+        sort(nums.begin(), nums.end());   // 🔴 MUST
+        vector<vector<int>> ans;
+        vector<int> ds;
+        vector<int> freq(nums.size(), 0);
+        solve(nums, ds, ans, freq);
         return ans;
     }
 };
