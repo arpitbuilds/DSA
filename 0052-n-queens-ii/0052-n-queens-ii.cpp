94335917e53e541582af425415bd65e4a20cf3fa
Arpit Singh <arpitbuilds@gmail.com>
2026-02-10 15:09:11 +0530

    Time: 3 ms (40.9%), Space: 9.5 MB (14.68%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -1,52 +1,50 @@
 class Solution {
 public:
-  bool can(int r , int c ,vector<string>&b,int n){
-        int tr=r;
-        int tc=c;
-        while(r>=0 && c>=0){
-            if(b[r--][c--]=='Q'){
+    bool check(int r, int c, vector<string>& v, int n) {
+        int tr = r;
+        int tc = c;
+        while (r >= 0 && c >= 0) {
+            if (v[r--][c--] == 'Q') {
                 return false;
             }
         }
-        r=tr;
-        c=tc;
-        while(c>=0){
-            if(b[r][c--]=='Q'){
+        r = tr;
+        c = tc;
+        while (c >= 0) {
+            if (v[r][c--] == 'Q') {
                 return false;
             }
         }
-        r=tr;
-        c=tc;
-        while(r<n && c>=0){
-            if(b[r++][c--]=='Q'){
+        r = tr;
+        c = tc;
+        while (r < n && c >= 0) {
+            if (v[r++][c--] == 'Q') {
                 return false;
             }
         }
         return true;
-     }
-     void solve(int c, int &cnt,vector<string>&b,int n ){
-        if(c==n){
-           cnt++;
+    }
+    void solve(int c, int& cnt, int n, vector<string>& v, string s) {
+        if (c == n) {
+            cnt++;
             return;
         }
-        for(int r=0;r<n;r++){
-            if(can(r,c,b,n)){
-                b[r][c]='Q';
-                solve(c+1,cnt,b,n);
-                b[r][c]='.';
+        for (int r = 0; r < n; r++) {
+            if (check(r, c, v, n)) {
+                v[r][c] = 'Q';
+                solve(c + 1, cnt, n, v, s);
+                v[r][c] = '.';
             }
         }
-
-     }
+    }
     int totalNQueens(int n) {
-        int cnt=0;
-         vector<string>b(n);
-         string s(n,'.');
-         for(int i=0;i<n;i++){
-            b[i]=s;
-         }
-         solve(0,cnt,b,n);
-         return cnt;
-        
+        string s(n, '.');
+        vector<string>v(n);
+        for (int i = 0; i < n; i++) {
+            v[i] = s;
+        }
+        int cnt = 0;
+        solve(0, cnt, n, v, s);
+        return cnt;
     }
 };
