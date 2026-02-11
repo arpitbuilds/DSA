debb0cc76f95dc934644ba42dee2a960e7d9881f
Arpit Singh <arpitbuilds@gmail.com>
2026-02-11 15:53:02 +0530

    Time: 2 ms (71.11%), Space: 11.6 MB (73.44%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,38 +1,31 @@
 class Solution {
 public:
-    void solve(vector<int>& nums, vector<int>& ds,
-               vector<vector<int>>& ans, vector<int>& freq) {
-
-        if (ds.size() == nums.size()) {
+    void solve(vector<vector<int>>&ans,vector<int>&ds,vector<int>&freq,vector<int>&nums){
+        if(ds.size()==nums.size()){
             ans.push_back(ds);
             return;
         }
-
-        for (int i = 0; i < nums.size(); i++) {
-
-            if (freq[i] == 0) {
-
-                // TAKE
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            if(freq[i]==0){
+                freq[i]=1;
                 ds.push_back(nums[i]);
-                freq[i] = 1;
-                solve(nums, ds, ans, freq);
-                freq[i] = 0;
+                solve(ans,ds,freq,nums);
+                freq[i]=0;
                 ds.pop_back();
-
-                // 🔥 DUPLICATE SKIP (exactly like subsets / comb sum 2)
-                while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
+                while(i+1<nums.size()  && nums[i]==nums[i+1]){
                     i++;
                 }
             }
         }
     }
-
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        sort(nums.begin(), nums.end());   // 🔴 MUST
-        vector<vector<int>> ans;
-        vector<int> ds;
-        vector<int> freq(nums.size(), 0);
-        solve(nums, ds, ans, freq);
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        vector<vector<int>>ans;
+        vector<int>ds;
+        vector<int>freq(n,0);
+        solve(ans,ds,freq,nums);
         return ans;
     }
 };
