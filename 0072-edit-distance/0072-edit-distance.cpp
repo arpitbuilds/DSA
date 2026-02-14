1e29e05d0a599b736cdb4458263589e88784ab01
Arpit Singh <arpitbuilds@gmail.com>
2026-02-14 12:40:00 +0530

    Time: 71 ms (6.51%), Space: 84.7 MB (5.3%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,27 +1,27 @@
 class Solution {
 public:
-    int solve(string w ,string t , int i,int j,vector<vector<int>>&dp){
-        if(i<0){
-        return j+1;
+    int solve(int i, int j, string w1, string w2, vector<vector<int>>& dp) {
+        if (i < 0) {
+            return j + 1;
         }
-        if(j<0){
-            return i+1;
+        if (j < 0) {
+            return i + 1;
         }
-        if(dp[i][j]!=-1){
+        if (dp[i][j] != -1) {
             return dp[i][j];
         }
-        if(w[i]==t[j]){
-            return dp[i][j]=0+solve(w,t,i-1,j-1,dp);
+        if (w1[i] == w2[j]) {
+            return dp[i][j] = 0 + solve(i - 1, j - 1, w1, w2,dp);
+        } else {
+            return dp[i][j] = 1 + min({solve(i - 1, j, w1, w2,dp),
+                                       solve(i - 1, j - 1, w1, w2,dp),
+                                       solve(i, j - 1, w1, w2,dp)});
         }
-        int op1=1+solve(w,t,i,j-1,dp);
-        int op2=1+solve(w,t,i-1,j,dp);
-        int op3=1+solve(w,t,i-1,j-1,dp);
-        return dp[i][j]=min({op1,op2,op3});
     }
-    int minDistance(string w, string t) {
-        int m=w.size();
-        int n=t.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return solve(w,t,m,n,dp);
+    int minDistance(string w1, string w2) {
+        int m = w1.size();
+        int n = w2.size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return solve(m - 1, n - 1, w1, w2, dp);
     }
 };
