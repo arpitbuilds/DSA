047707f9c1ca407698d52fe27f60910291b3a418
Arpit Singh <arpitbuilds@gmail.com>
2026-02-15 15:08:11 +0530

    Time: 3 ms (6.31%), Space: 10.7 MB (44.6%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,34 +1,22 @@
 class Solution {
 public:
-    // int solve(int ind,vector<int>&nums,vector<int>&dp){
-    //     if(ind==0){
-    //         return nums[ind];
-    //     }
-    //     if(ind<0){
-    //         return 0;
-    //     }
-    //     if(dp[ind]!=-1){
-    //         return dp[ind];
-    //     }
-    //     int pick=solve(ind-2,nums,dp)+nums[ind];
-    //     int npick=0+solve(ind-1,nums,dp);
-    //     return dp[ind]=max(pick,npick);
-    // }
-    int rob(vector<int>& nums) {
-        int n=nums.size();
-    //    vector<int>dp(n,0); 
-        int prev=nums[0];
-        int prev2=0;
-        for(int ind=1;ind<n;ind++){
-            int pick=nums[ind];
-            if(ind>1){
-                pick+=prev2;
-            }
-            int npick=0+prev;
-            int curi=max(pick,npick);
-            prev2=prev;
-            prev=curi;
+    int solve(int ind, vector<int>& nums, vector<int>& dp) {
+        if (ind < 0) {
+            return 0;
         }
-        return prev;
+        if (ind == 0) {
+            return nums[ind];
+        }
+        if (dp[ind] != -1) {
+            return dp[ind];
+        }
+        int op1 = nums[ind] + solve(ind - 2, nums, dp);
+        int op2 = 0 + solve(ind - 1, nums, dp);
+        return dp[ind] = max(op1, op2);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return solve(n - 1, nums, dp);
     }
 };
