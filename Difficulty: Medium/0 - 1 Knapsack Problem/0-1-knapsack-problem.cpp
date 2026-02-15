5b96322c145962f28372aa8bc47e6f0810c7cd85
Arpit Singh <arpitbuilds@gmail.com>
2026-02-15 13:35:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,43 +1,29 @@
 class Solution {
   public:
-    // int solve(int ind ,int mw,vector<int> &wt,vector<int> &val,vector<vector<int>>&dp){
-    //     if(ind==0){
-    //         if(wt[0]<=mw){
-    //             return val[0];
-    //         }
-    //         else{
-    //             return 0;
-    //         }
-    //     }
-    //     if(dp[ind][mw]!=-1){
-    //         return dp[ind][mw];
-    //     }
-    //     int ntake=0 + solve(ind-1,mw,wt,val,dp);
-    //     int take=INT_MIN;
-    //     if(wt[ind]<=mw){
-    //         take=val[ind]+solve(ind-1,mw-wt[ind],wt,val,dp);
-    //     }
-    //     return dp[ind][mw]= max(ntake,take);
-    // }
-    int knapsack(int W, vector<int> &val, vector<int> &wt) {
-        // code here
-        int n=val.size();
+    int solve(int ind,vector<int>&wt,vector<int>&val,int W,vector<vector<int>>&dp){
         
-        vector<vector<int>>dp(n,vector<int>(W+1,0));
-        for( int i=wt[0];i<=W;i++){
-            dp[0][i]=val[0];
-        }
-        for(int ind=1;ind<n;ind++){
-            for(int w=0;w<=W;w++){
-                int ntake=0+dp[ind-1][w];
-                int take=INT_MIN;
-                if(wt[ind]<=w){
-                    take=val[ind]+dp[ind-1][w-wt[ind]];
-                }
-                dp[ind][w]=max(take,ntake);
+        if(ind==0){
+            if(wt[ind]<=W){
+                return val[ind];
+            }
+            else{
+                return 0;
             }
         }
-        return dp[n-1][W];
-        
+        if(dp[ind][W]!=-1){
+            return dp[ind][W];
+        }
+        int take=0;
+        if(wt[ind]<=W){
+            take=val[ind]+solve(ind-1,wt,val,W-wt[ind],dp);
+        }
+        int ntake=0+solve(ind-1,wt,val,W,dp);
+        return dp[ind][W]=max(take,ntake);
+    }
+    int knapsack(int W, vector<int> &val, vector<int> &wt) {
+        // code here
+        int n=wt.size();
+        vector<vector<int>>dp(n,vector<int>(W+1,-1));
+        return solve(n-1,wt,val,W,dp);
     }
 };
