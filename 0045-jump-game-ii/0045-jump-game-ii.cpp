d4757f167a6e9db589612768abeb402972f47619
Arpit Singh <arpitbuilds@gmail.com>
2026-02-16 09:58:07 +0530

    Time: 0 ms (100%), Space: 20.4 MB (83.64%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -4,14 +4,14 @@ public:
         int n = nums.size();
         int c = 0;
         int far = 0;
-        int j = 0;
+        int jump = 0;
         for (int i = 0; i < n - 1; i++) {
             far = max(far, i + nums[i]);
             if (i == c) {
-                j++;
+                jump++;
                 c = far;
             }
         }
-        return j;
+        return jump;
     }
 };
