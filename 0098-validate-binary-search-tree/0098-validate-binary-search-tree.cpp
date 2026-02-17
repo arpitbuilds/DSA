cb5e8e4e5e1fc6b7f65e002003bb070065e23115
Arpit Singh <arpitbuilds@gmail.com>
2026-02-17 18:11:02 +0530

    Time: 0 ms (100%), Space: 21.9 MB (73.93%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.cpp
@@ -6,36 +6,25 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool solve(TreeNode* root, long long& prev) {
-        if (root == NULL) {
+    bool solve(TreeNode*root, long long &prev){
+        if(root==NULL){
             return true;
         }
-
-        bool left = solve(root->left, prev);
-
-        if (root->val <= prev) {   // changed < to <=
+        bool left=solve(root->left,prev);
+        if(root->val<=prev){
             return false;
         }
-
-        prev = root->val;
-
-        bool right = solve(root->right, prev);
-
+        prev=root->val;
+        bool right=solve(root->right,prev);
         return left && right;
     }
-
     bool isValidBST(TreeNode* root) {
-        if (root == NULL) {
-            return true;
-        }
-
-        long long prev = LLONG_MIN;   // changed from -1 to LLONG_MIN
-        return solve(root, prev);
+        long long prev=LLONG_MIN;
+        return solve(root,prev);
     }
 };
