d408b0c61ba4c4636b62a32365a99432dbb6d3a9
Arpit Singh <arpitbuilds@gmail.com>
2026-03-24 21:55:37 +0530

    Time: 3 ms (61.68%), Space: 11.4 MB (23.48%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-    bool check(int r, int c, vector<string>& v, int n) {
+    bool safe(int r, int c, vector<string>&ans,int n) {
         int tr = r;
         int tc = c;
         while (r >= 0 && c >= 0) {
-            if (v[r--][c--] == 'Q') {
+            if (ans[r--][c--] == 'Q') {
                 return false;
             }
         }
         r = tr;
         c = tc;
         while (c >= 0) {
-            if (v[r][c--] == 'Q') {
+            if (ans[r][c--] == 'Q') {
                 return false;
             }
         }
         r = tr;
         c = tc;
         while (r < n && c >= 0) {
-            if (v[r++][c--] == 'Q') {
+            if (ans[r++][c--] == 'Q') {
                 return false;
             }
         }
         return true;
     }
-    void solve(int c, int n, string s,vector<string>&v, vector<vector<string>>& ans) {
+    void solve(int c, vector<string>& v, string s, vector<vector<string>>& ans,
+               int n) {
         if (c == n) {
             ans.push_back(v);
             return;
         }
         for (int r = 0; r < n; r++) {
-            if (check(r, c, v,n)) {
+            if (safe(r, c, v,n)) {
                 v[r][c] = 'Q';
-                solve(c + 1, n, s,v,ans);
+                solve(c + 1, v, s, ans, n);
                 v[r][c] = '.';
             }
         }
@@ -44,7 +45,7 @@ public:
         for (int i = 0; i < n; i++) {
             v[i] = s;
         }
-        solve(0, n, s,v, ans);
+        solve(0, v, s, ans, n);
         return ans;
     }
 };
