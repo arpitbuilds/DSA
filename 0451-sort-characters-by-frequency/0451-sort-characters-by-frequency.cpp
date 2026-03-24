9ec0cf8ba9d65f4500aaa67cd2d8637c8a287431
Arpit Singh <arpitbuilds@gmail.com>
2026-03-24 10:30:58 +0530

    Time: 4 ms (38.07%), Space: 11.3 MB (43.59%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,26 +1,20 @@
-#include <string>
-#include <unordered_map>
-#include <vector>
-#include <algorithm>
-using namespace std;
-
-bool compare(pair<char,int>&a,pair<char,int>&b){
-    return a.second>b.second;
-}
-
 class Solution {
 public:
+   static bool compare(pair<char,int>&a,pair<char,int>&b){
+        return a.second>b.second;
+    }
     string frequencySort(string s) {
-     unordered_map<char,int>freq;
-     for(auto c:s){
-       freq[c]++;
-     }
-     vector<pair<char,int>>v(freq.begin(),freq.end());
-     sort(v.begin(),v.end(),compare);
-     string res;
-     for(auto p : v){
-        res.append(p.second,p.first);
-     }
-     return res;
+        unordered_map<char,int>mpp;
+        for(auto ch : s){
+            mpp[ch]++;
+        }
+        vector<pair<char,int>>v(mpp.begin(),mpp.end());
+        sort(v.begin(),v.end(),compare);
+        string res="";
+        for(auto p : v){
+            res.append(p.second,p.first);
+        }
+        return res;
+
     }
 };
