1d82820475d0f62c71473822791e65f7a0c5b083
Arpit Singh <arpitbuilds@gmail.com>
2026-03-30 20:47:10 +0530

    Time: 18 ms (20.05%), Space: 22.5 MB (21.88%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,51 +1,52 @@
 class Solution {
 public:
-    int solve(vector<int>&v){
-        int n=v.size();
+      int solve(vector<int>& h) {
+        stack<int>st;
+        stack<int>st2;
+        int n=h.size();
         vector<int>pse(n);
         vector<int>nse(n);
-        stack<int>st;
-        for(int i=0;i<n;i++){
-            while(!st.empty() && v[st.top()]>=v[i]){
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && h[st.top()]>=h[i]){
                 st.pop();
             }
-            pse[i]=st.empty()?-1:st.top();
+            nse[i]=st.empty()?n:st.top();
             st.push(i);
+
         }
-             stack<int>st2;
-        for(int i=n-1;i>=0;i--){
-            while(!st2.empty() && v[st2.top()]>v[i]){
+        for(int i=0;i<n;i++){
+            while(!st2.empty() && h[st2.top()]>=h[i]){
                 st2.pop();
             }
-            nse[i]=st2.empty()?n:st2.top();
+            pse[i]=st2.empty()?-1:st2.top();
             st2.push(i);
         }
-       int maxi=INT_MIN;
-       for(int i=0;i<n;i++){
-        int r=nse[i];
-        int l=pse[i];
-        int w=r-l-1;
-        maxi=max(maxi,w*v[i]);
-       }
-       return maxi;
+        int maxi=0;
+        for(int i=0;i<n;i++){
+            int left=pse[i];
+            int right=nse[i];
+            int w=right-left-1;
+            maxi=max(maxi,w*h[i]);
+        }
+        return maxi;
     }
+
     int maximalRectangle(vector<vector<char>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
-        int res=0;
+        int maxi=0;
         vector<int>v(n,0);
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(matrix[i][j]=='1'){
-                    v[j]+=1;
+                    v[j]++;
                 }
                 else{
-                 v[j]=0;
+                    v[j]=0;
                 }
             }
-         res=max(res,solve(v));
+            maxi=max(maxi,solve(v));
         }
-   
-    return res;
+        return maxi;
     }
 };
