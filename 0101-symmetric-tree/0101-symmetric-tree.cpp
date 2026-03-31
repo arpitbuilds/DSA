ac6370aa711e09b5db71fb5316d232404c403be7
Arpit Singh <arpitbuilds@gmail.com>
2026-03-31 12:59:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -6,18 +6,19 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
-    bool check(TreeNode* a, TreeNode* b) {
-        if (a == NULL || b == NULL) {
-            return a == b;
+     bool solve(TreeNode* p, TreeNode* q) {
+        if (p == NULL || q == NULL) {
+            return p == q;
         }
-        return (a->val == b->val) && check(a->left, b->right) &&
-               check(a->right, b->left);
+        return (p->val == q->val) && solve(p->left, q->right) &&
+               solve(p->left, q->right);
     }
-    bool isSymmetric(TreeNode* root) { return check(root, root); }
+    bool isSymmetric(TreeNode* root) {
+        return solve(root,root);
+    }
 };
