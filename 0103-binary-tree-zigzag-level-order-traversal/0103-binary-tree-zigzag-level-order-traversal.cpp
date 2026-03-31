101824746bb9ae9f6096a07e66af014083f2d4f5
Arpit Singh <arpitbuilds@gmail.com>
2026-03-31 13:17:37 +0530

    Time: 0 ms (100%), Space: 15.2 MB (17.57%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -6,42 +6,42 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>> ans;
-        if (root == NULL) {
+        vector<vector<int>>ans;
+        if(root==NULL){
             return ans;
         }
-        bool flag = 1;
-        queue<TreeNode*> q;
+        queue<TreeNode*>q;
         q.push(root);
-        while (!q.empty()) {
-            int s = q.size();
-            vector<int> res(s);
-            for (int i = 0; i < s; i++) {
-                TreeNode* x = q.front();
+        bool flag=true;
+        int ind;
+        while(!q.empty()){
+            int s=q.size();
+            vector<int>v(s);
+            for(int i=0;i<s;i++){
+                TreeNode*x=q.front();
                 q.pop();
-                int ind;
-                if (x->left) {
+                if(flag){
+                    ind=i;
+                }
+                else{
+                    ind=s-i-1;
+                }
+                v[ind]=x->val;
+                if(x->left){
                     q.push(x->left);
                 }
-                if (x->right) {
+                if(x->right){
                     q.push(x->right);
                 }
-                if (flag) {
-                    ind = i;
-                } else {
-                    ind = s - i - 1;
-                }
-                res[ind] = x->val;
             }
-            flag = !flag;
-            ans.push_back(res);
+            ans.push_back(v);
+            flag=!flag;
         }
         return ans;
     }
