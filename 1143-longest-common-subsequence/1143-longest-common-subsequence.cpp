b0cc7feaf58c5d7f9d623e2a53a62166747633bc
Arpit Singh <arpitbuilds@gmail.com>
2026-04-03 21:47:41 +0530

    Time: 26 ms (65.76%), Space: 27.4 MB (51.29%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
-    int solve(int m,int n,string &s,string &t,vector<vector<int>>&dp){
-        if(m==0 || n==0){
-            return 0;
-        }
-        if(dp[m][n]!=-1){
-            return dp[m][n];
-        }
-        if(s[m-1]==t[n-1]){
-            return dp[m][n]=1+solve(m-1,n-1,s,t,dp);
-        }
-        else{
-            return dp[m][n]=max(solve(m-1,n,s,t,dp),solve(m,n-1,s,t,dp));
-        }
-    }
+    // int solve(int m,int n,string &s,string &t,vector<vector<int>>&dp){
+    //     if(m==0 || n==0){
+    //         return 0;
+    //     }
+    //     if(dp[m][n]!=-1){
+    //         return dp[m][n];
+    //     }
+    //     if(s[m-1]==t[n-1]){
+    //         return dp[m][n]=1+solve(m-1,n-1,s,t,dp);
+    //     }
+    //     else{
+    //         return dp[m][n]=max(solve(m-1,n,s,t,dp),solve(m,n-1,s,t,dp));
+    //     }
+    // }
     int longestCommonSubsequence(string s, string t) {
-        int m=s.size();
-        int n=t.size();
-        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
-        return solve(m,n,s,t,dp);
+        int m = s.size();
+        int n = t.size();
+        // vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 0;
+        }
+        for (int i = 1; i <=m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (s[i- 1] == t[j - 1]) {
+                     dp[i][j] = 1 + dp[i - 1][j- 1];
+                } else {
+                    dp[i][j] = max(dp[i - 1][j],
+                                          dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[m][n];
+       // return solve(m, n, s, t, dp);
     }
 };
