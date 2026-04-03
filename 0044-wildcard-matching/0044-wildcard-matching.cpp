05bedc3dd598a4001360d07f6f573f9967c1b2ae
Arpit Singh <arpitbuilds@gmail.com>
2026-04-04 00:04:12 +0530

    Time: 43 ms (64.31%), Space: 54.2 MB (28.67%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -4,6 +4,9 @@ public:
         if (m < 0 && n < 0) {
             return true;
         }
+        if (m >= 0 && n < 0) {
+            return false;
+        }
         if (m < 0 && n >= 0) {
             for (int k = 0; k <= n; k++) {
                 if (p[k] != '*') {
@@ -12,15 +15,11 @@ public:
             }
             return true;
         }
-        if (m >= 0 && n < 0) {
-            return false;
-        }
         if(dp[m][n]!=-1){
             return dp[m][n];
         }
-
-        if (s[m] == p[n] || p[n] == '?') {
-            return dp[m][n]=solve(m - 1, n - 1, s, p,dp);
+        if (p[n] == '?' || s[m] == p[n]) {
+            return  dp[m][n]=solve(m - 1, n - 1, s, p,dp);
         }
         if (p[n] == '*') {
             return dp[m][n]=solve(m - 1, n, s, p,dp) || solve(m, n - 1, s, p,dp);
