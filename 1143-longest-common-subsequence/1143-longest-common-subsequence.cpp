d45781d5da79c4f40826ef87f872a61b2693094f
Arpit Singh <arpitbuilds@gmail.com>
2026-04-03 14:58:18 +0530

    Time: 46 ms (14.17%), Space: 27.6 MB (14.93%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
-    // int solve(int i, int j, string s, string t, vector<vector<int>>& dp) {
-    //     if (i == 0 || j == 0) {
-    //         return 0;
-    //     }
-    //     if (dp[i][j] != -1) {
-    //         return dp[i][j];
-    //     }
-    //     if (s[i - 1] == t[j - 1]) {
-    //         return dp[i][j] = 1 + solve(i - 1, j - 1, s, t, dp);
-    //     } else {
-    //         return dp[i][j] = 0 + max(solve(i - 1, j, s, t, dp),
-    //                                   solve(i, j - 1, s, t, dp));
-    //     }
-    // }
+    int solve(int m,int n,string &s,string &t,vector<vector<int>>&dp){
+        if(m==0 || n==0){
+            return 0;
+        }
+        if(dp[m][n]!=-1){
+            return dp[m][n];
+        }
+        if(s[m-1]==t[n-1]){
+            return dp[m][n]=1+solve(m-1,n-1,s,t,dp);
+        }
+        else{
+            return dp[m][n]=max(solve(m-1,n,s,t,dp),solve(m,n-1,s,t,dp));
+        }
+    }
     int longestCommonSubsequence(string s, string t) {
-        int l = s.length();
-        int m = t.length();
-        vector<vector<int>> dp(l + 1, vector<int>(m + 1, 0));
-        // return solve(l , m , s, t,dp);
-        for (int i = 0; i <= l; i++) {
-            dp[i][0] = 0;
-        }
-        for (int j = 0; j <= m; j++) {
-            dp[0][j] = 0;
-        }
-        for (int i = 1; i <= l; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (s[i - 1] == t[j - 1]) {
-                    dp[i][j] = 1 + dp[i - 1][j - 1];
-                } else {
-                    dp[i][j] = 0 + max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
-        return dp[l][m];
+        int m=s.size();
+        int n=t.size();
+        vector<vector<int>>dp(m+1,vector<int>(n+1,-1));
+        return solve(m,n,s,t,dp);
     }
 };
