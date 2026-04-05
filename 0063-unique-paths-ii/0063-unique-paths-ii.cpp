c4ffd9da21036c2a73398c817c9cdcd77e3901b0
Arpit Singh <arpitbuilds@gmail.com>
2026-04-05 12:01:23 +0530

    Time: 0 ms (100%), Space: 11.9 MB (29.01%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
-     int solve(int i , int j,vector<vector<int>>&dp,vector<vector<int>>&g){
+    int solve(int i,int j,vector<vector<int>>&grid,vector<vector<int>>&dp){
         if(i<0 || j<0){
             return 0;
         }
-        if(g[i][j]==1){
+        if(grid[i][j]==1){
             return 0;
         }
         if(i==0 && j==0){
@@ -13,14 +13,14 @@ public:
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        int left=solve(i-1,j,dp,g);
-        int right=solve(i,j-1,dp,g);
-        return dp[i][j]=left+right;
+        int op1=solve(i-1,j,grid,dp);
+        int op2=solve(i,j-1,grid,dp);
+        return dp[i][j]= op1+op2;
     }
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-         vector<vector<int>>dp(m,vector<int>(n,-1));
-        return solve(m-1,n-1,dp,grid);
+    int uniquePathsWithObstacles(vector<vector<int>>&g) {
+        int m=g.size();
+        int n=g[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,g,dp);
     }
 };
