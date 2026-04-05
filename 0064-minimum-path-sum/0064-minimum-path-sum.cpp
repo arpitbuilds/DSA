5893ef3a795ac51e496dff9cde8844a988d4e286
Arpit Singh <arpitbuilds@gmail.com>
2026-04-05 12:06:00 +0530

    Time: 0 ms (100%), Space: 16.5 MB (15.14%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,44 +1,23 @@
 class Solution {
 public:
-    // int solve(vector<vector<int>>& grid, vector<vector<int>>& dp, int i,
-    //           int j) {
-    //     if (i < 0 || j < 0) {
-    //         return INT_MAX;
-    //     }
-    //     if (i == 0 && j == 0) {
-    //         return grid[0][0];
-    //     }
-    //     if (dp[i][j] != -1) {
-    //         return dp[i][j];
-    //     }
-    //     int up = solve(grid, dp, i - 1, j);
-
-    //     int left = solve(grid, dp, i, j - 1);
-
-    //     return dp[i][j] = grid[i][j] + min(up, left);
-    // }
-    int minPathSum(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        vector<vector<int>> dp(m, vector<int>(n, 0));
-        // return solve(grid,dp,m-1,n-1);
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) {
-                    dp[i][j] = grid[0][0];
-                } else {
-                    int left = INT_MAX;
-                    int up = INT_MAX;
-                    if (i > 0) {
-                        up = dp[i - 1][j];
-                    }
-                    if (j > 0) {
-                        left = dp[i][j - 1];
-                    }
-                    dp[i][j] =grid[i][j]+ min(left, up);
-                }
-            }
+      int solve(int i,int j,vector<vector<int>>&grid,vector<vector<int>>&dp){
+       if(i<0 || j<0){
+            return INT_MAX;
         }
-        return dp[m - 1][n - 1];
+        if(i==0 && j==0){
+            return grid[0][0];
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int op1=solve(i-1,j,grid,dp);
+        int op2=solve(i,j-1,grid,dp);
+        return dp[i][j]=grid[i][j]+ min(op1,op2);
+    }
+    int minPathSum(vector<vector<int>>& g) {
+        int m=g.size();
+        int n=g[0].size();
+        vector<vector<int>>dp(m,vector<int>(n,-1));
+        return solve(m-1,n-1,g,dp);
     }
 };
