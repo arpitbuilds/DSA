e3081c7c8633f5c26624a503cb4a68b6aaa755ff
Arpit Singh <arpitbuilds@gmail.com>
2026-04-08 16:29:04 +0530

    Time: 0 ms (100%), Space: 9.4 MB (14.41%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,37 +1,37 @@
 class Solution {
 public:
-    vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        int m=matrix.size();
-        int n=matrix[0].size();
-        int sr=0;
-        int sc=0;
-        int er=m-1;
-        int ec=n-1;
-        int tot=m*n;
-        int cnt=0;
-        vector<int>ans;
-        while(cnt<tot){
-            for(int j=sc;j<=ec &&  cnt < tot;j++){
-                ans.push_back(matrix[sr][j]);
+    vector<int> spiralOrder(vector<vector<int>>& mat) {
+        vector<int> ans;
+        int m = mat.size();
+        int n = mat[0].size();
+        int tot = m * n;
+        int cnt = 0;
+        int sr = 0;
+        int sc = 0;
+        int er = m - 1;
+        int ec = n - 1;
+        while (cnt < tot) {
+            for (int i = sc; i <= ec && cnt < tot; i++) {
+                ans.push_back(mat[sr][i]);
                 cnt++;
             }
             sr++;
-            for(int j=sr;j<=er &&  cnt < tot;j++){
-            ans.push_back(matrix[j][ec]);
-            cnt++;
+            for (int i = sr; i <= er && cnt < tot; i++) {
+                ans.push_back(mat[i][ec]);
+                cnt++;
             }
             ec--;
-            for(int j=ec;j>=sc &&  cnt < tot;j--){
-                ans.push_back(matrix[er][j]);
+            for (int i = ec; i >= sc && cnt < tot; i--) {
+                ans.push_back(mat[er][i]);
                 cnt++;
             }
             er--;
-            for(int j=er;j>=sr &&  cnt < tot;j--){
-                ans.push_back(matrix[j][sc]);
+            for (int i = er; i >= sr && cnt < tot; i--) {
+                ans.push_back(mat[i][sc]);
                 cnt++;
             }
             sc++;
         }
-  return ans;
+        return ans;
     }
 };
