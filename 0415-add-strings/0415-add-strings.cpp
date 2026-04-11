0a043e5d92bbf67807f9f4dc40057162229a5261
Arpit Singh <arpitbuilds@gmail.com>
2026-04-11 10:53:24 +0530

    Time: 0 ms (100%), Space: 9.2 MB (59.13%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string addStrings(string num1, string num2) {
+        int i=num1.size()-1;
+        int j=num2.size()-1;
+        int c=0;
+        string res="";
+        while(i>=0 || j>=0 || c){
+            int sum=c;
+            if(i>=0){
+                sum+=num1[i]-'0';
+                i--;
+            }
+            if(j>=0){
+                sum+=num2[j]-'0';
+                j--;
+            }
+            res+=(sum%10)+'0';
+            c=sum/10;
+        }
+        reverse(res.begin(),res.end());
+        return res;
+    }
+};
