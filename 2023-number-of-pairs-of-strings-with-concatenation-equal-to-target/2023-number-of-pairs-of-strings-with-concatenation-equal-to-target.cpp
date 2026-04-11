61507960482d923a6a6d8d711ea9c4eab8787435
Arpit Singh <arpitbuilds@gmail.com>
2026-04-11 13:29:09 +0530

    Time: 3 ms (84.53%), Space: 14.1 MB (78.25%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.cpp
@@ -1,12 +1,27 @@
 class Solution {
 public:
     int numOfPairs(vector<string>& nums, string target) {
-        int n=nums.size();
+        // int n=nums.size();
+        // int cnt=0;
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(i!=j && nums[i]+nums[j]==target){
+        //             cnt++;
+        //         }
+        //     }
+        // }
+        // return cnt;
+        unordered_map<string,int>mpp;
+        for(auto it : nums){
+            mpp[it]++;
+        }
         int cnt=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(i!=j && nums[i]+nums[j]==target){
-                    cnt++;
+        for(auto &s : nums){
+            if(target.substr(0,s.size())==s){
+                string suffix=target.substr(s.size());
+                cnt+=mpp[suffix];
+                if(suffix==s){
+                    cnt--;
                 }
             }
         }
