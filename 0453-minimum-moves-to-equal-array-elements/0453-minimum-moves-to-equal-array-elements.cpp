30af249c3754549642b74c37ee82c4a9ec4250cd
Arpit Singh <arpitbuilds@gmail.com>
2026-04-16 20:27:55 +0530

    Time: 3 ms (27.63%), Space: 31.9 MB (89.48%) - LeetHub

---
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int minMoves(vector<int>& nums) {
+        int mini=*min_element(nums.begin(),nums.end());
+        int ans=0;
+        for(int i=0;i<nums.size();i++){
+            ans+=nums[i]-mini;
+        }
+        return ans;
+    }
+};
