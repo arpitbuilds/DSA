e794a066cd7860cb75c8c464b7b1b4752acaab05
Arpit Singh <arpitbuilds@gmail.com>
2026-04-19 23:56:44 +0530

    Time: 4 ms (23.28%), Space: 112.5 MB (59.84%) - LeetHub

---
+++ 0134-gas-station/0134-gas-station.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int tgas=0;
+        int tcost=0;
+        for(auto it : gas){
+            tgas+=it;
+        }
+        for(auto it : cost){
+            tcost+=it;
+        }
+         if(tgas < tcost) return -1;
+        int cgas=0;
+        int sind=0;
+        for(int i=0;i<gas.size();i++){
+            cgas+=gas[i]-cost[i];
+            if(cgas<0){
+                sind=i+1;
+                cgas=0;
+            }
+        }
+        return sind;
+    }
+};
