1f4ce3b058f3dbc8c3746ea2e275a888d4fa4a2b
Arpit Singh <arpitbuilds@gmail.com>
2026-04-20 11:02:13 +0530

    Time: 4 ms (34.77%), Space: 32.1 MB (40.77%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int numPairsDivisibleBy60(vector<int>& time) {
-        int mpp[60]={};
-        int res=0;
+        unordered_map<int,int>mpp;
+        int ans=0;
         for(auto it : time){
             int t=it%60;
             int y=(60-t)%60;
-            res+=mpp[y];
+            ans+=mpp[y];
             mpp[t]++;
         }
-        return res;
+        return ans;
     }
 };
