cbceb69678fb76f82f6b3a6a65726071d837ec23
Arpit Singh <arpitbuilds@gmail.com>
2026-05-23 16:08:11 +0530

    Time: 0 ms (100%), Space: 11.2 MB (53.59%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -4,9 +4,9 @@ public:
         int n=arr.size();
         int cnt=0;
         for(int i=0;i<n;i++){
-            if(arr[i]>arr[(i+1)%n]){
-                cnt++;
-            }
+          if(arr[i]>arr[(i+1)%n]){
+            cnt++;
+          }
         }
         return cnt<=1;
     }
