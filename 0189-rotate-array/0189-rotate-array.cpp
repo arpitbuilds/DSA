d360972e0088d809787d895d64960805699f8911
Arpit Singh <arpitbuilds@gmail.com>
2026-05-25 14:01:54 +0530

    Time: 0 ms (100%), Space: 29.4 MB (91.29%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
+    void reversee(vector<int>&v,int s ,int e){
+        while(s<=e){
+            swap(v[s],v[e]);
+            s++;
+            e--;
+        }
+    }
     void rotate(vector<int>& nums, int k) {
         int n=nums.size();
-        vector<int>ans(n);
-        for(int i=0;i<n;i++){
-            ans[(i+k)%n]=nums[i];
-        }
-        nums=ans;
-       
+        k=k%n;
+        reversee(nums,0,n-1);
+        reversee(nums,0,k-1);
+        reversee(nums,k,n-1);
     }
 };
