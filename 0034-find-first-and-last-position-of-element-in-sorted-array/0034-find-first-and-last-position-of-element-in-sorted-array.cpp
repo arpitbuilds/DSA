a5e2f97c00490ab2a754771cd7123b0e3020fe79
Arpit Singh <arpitbuilds@gmail.com>
2026-05-26 16:21:04 +0530

    Time: 0 ms (100%), Space: 17.5 MB (88.1%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int fo(vector<int>&nums,int target){
-        int first=-1;
+
+        int lo(vector<int>&nums,int target){
+        int ans=-1;
         int l=0;
-        int n=nums.size();
-        int r=n-1;
+        int r=nums.size()-1;
         while(l<=r){
             int mid=l+(r-l)/2;
             if(nums[mid]==target){
-             first=mid;
-             r=mid-1;
+                ans=mid;
+                l=mid+1;
             }
             else if(nums[mid]<target){
                 l=mid+1;
@@ -18,18 +18,18 @@ public:
                 r=mid-1;
             }
         }
-        return first;
+        return ans;
     }
-     int lo(vector<int>&nums,int target){
-        int last=-1;
+
+    int fo(vector<int>&nums,int target){
+        int ans=-1;
         int l=0;
-        int n=nums.size();
-        int r=n-1;
+        int r=nums.size()-1;
         while(l<=r){
             int mid=l+(r-l)/2;
             if(nums[mid]==target){
-             last=mid;
-             l=mid+1;
+                ans=mid;
+                r=mid-1;
             }
             else if(nums[mid]<target){
                 l=mid+1;
@@ -38,14 +38,14 @@ public:
                 r=mid-1;
             }
         }
-        return last;
+        return ans;
     }
     vector<int> searchRange(vector<int>& nums, int target) {
         vector<int>res;
-        int first=fo(nums,target);
-        int last=lo(nums,target);
-        res.push_back(first);
-        res.push_back(last);
+        int foo=fo(nums,target);
+        int loo=lo(nums,target);
+        res.push_back(foo);
+        res.push_back(loo);
         return res;
     }
 };
