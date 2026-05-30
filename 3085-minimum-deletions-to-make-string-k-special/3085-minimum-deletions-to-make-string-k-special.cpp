6683dc9158a4a06fcba74ce0aac5eb7c45bff38f
Arpit Singh <arpitbuilds@gmail.com>
2026-05-30 22:25:34 +0530

    Time: 23 ms (24.19%), Space: 19.6 MB (36.02%) - LeetHub

--- 3085-minimum-deletions-to-make-string-k-special/3085-minimum-deletions-to-make-string-k-special.cpp
+++ 3085-minimum-deletions-to-make-string-k-special/3085-minimum-deletions-to-make-string-k-special.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
     int minimumDeletions(string word, int k) {
-
-        int ans = INT_MAX;
-
-        unordered_map<char,int> mpp;
-
+        unordered_map<char,int>mpp;
+        int ans=INT_MAX;
         for(auto c : word){
             mpp[c]++;
         }
-
         for(auto it : mpp){
-
-            int x = it.second;
-            int del = 0;
-
-            for(auto p : mpp){
-
-                int freq = p.second;
-
-                if(freq < x){
-                    del += freq;
+            int x=it.second;
+            int del=0;
+            for(auto itt : mpp){
+                int y=itt.second;
+                if(y<x){
+                 del+=y;
                 }
-                else if(freq > x + k){
-                    del += (freq - (x + k));
+                else if(y>x+k){
+                    del+=(y-(x+k));
                 }
             }
-
-            ans = min(ans, del);
+            ans=min(ans,del);
         }
-
         return ans;
     }
 };
