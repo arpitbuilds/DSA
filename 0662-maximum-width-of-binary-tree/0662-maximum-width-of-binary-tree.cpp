c526944a8f9a64635f67e7c6598b52af6a2ffaa4
Arpit Singh <arpitbuilds@gmail.com>
2026-06-06 03:56:48 +0530

    Time: 0 ms (100%), Space: 19.9 MB (12.7%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -6,31 +6,27 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        queue<pair<TreeNode*,  unsigned long long>> q;
-        q.push({root, 0});
-        unsigned long long l, r;
-        int maxi = 0;
-        if(root==NULL){
-            return 0;
-        }
-        while (!q.empty()) {
-            int s = q.size();
-            for (int i = 0; i < s; i++) {
-                TreeNode* x = q.front().first;
-               unsigned long long ind = q.front().second;
+        queue<pair<TreeNode*,unsigned long long>>q;
+        q.push({root,0});
+        unsigned long long l,r;
+       int maxi=INT_MIN;
+        while(!q.empty()){
+            int s=q.size();
+            for(int i=0;i<s;i++){
+                TreeNode*x=q.front().first;
+                unsigned long long ind=q.front().second;
                 q.pop();
-                if (x->left) {
-                    q.push({x->left, 2 * ind + 1});
+                if(x->left){
+                    q.push({x->left,2*ind+1});
                 }
-                if (x->right) {
-                    q.push({x->right, 2 * ind + 2});
+                if(x->right){
+                    q.push({x->right,2*ind+2});
                 }
                 if(i==0){
                     l=ind;
@@ -39,7 +35,7 @@ public:
                     r=ind;
                 }
             }
-            maxi = max(maxi, (int)(r - l + 1));
+            maxi=max(maxi,(int)(r-l+1));
         }
         return maxi;
     }
