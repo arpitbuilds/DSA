bc2dccbfd336b1f0acd2cf8cb19ead287f4352b2
Arpit Singh <arpitbuilds@gmail.com>
2026-06-06 18:00:13 +0530

    Time: 0 ms (100%), Space: 8.9 MB (66.82%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
     bool isValid(string s) {
-        stack<int>st;
-        for(auto it : s){
-            if(it=='(' || it=='[' || it=='{'){
-                st.push(it);
+        int n=s.size();
+        stack<char>st;
+        for(int i=0;i<n;i++){
+            if(s[i]=='[' || s[i]=='('  || s[i]=='{'){
+                st.push(s[i]);
             }
             else{
-                if(st.empty()){
-                    return false;
-                }
-                if((st.top()=='[' && it==']') || (st.top()=='{' && it=='}') || (st.top()=='(' && it==')')){
-                    st.pop();
-                }
-                
+              if(st.empty() ||
+   (s[i] == ')' && st.top() != '(') ||
+   (s[i] == ']' && st.top() != '[') ||
+   (s[i] == '}' && st.top() != '{'))
+{
+    return false;
+}
                 else{
-                    return false;
+                    st.pop();
                 }
             }
         }
-        if(st.empty()){
-            return true;
-        }
-        return false;
+        return st.size()==0;
     }
 };
