556568568a4437a33e180511f4810f3a5a085005
Arpit Singh <arpitbuilds@gmail.com>
2026-06-07 14:28:10 +0530

    Time: 0 ms (100%), Space: 23.7 MB (81.83%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -5,14 +5,14 @@ public:
         int n=nums.size();
         for(int i=0;i<n;i++){
             if(nums[i]!=0){
-                nums[k]=nums[i];
+                swap(nums[k],nums[i]);
                 k++;
             }
         }
-        while(k<n){
-            nums[k]=0;
-            k++;
-        }
+        // while(k<n){
+        //     nums[k]=0;
+        //     k++;
+        // }
 
     }
 };
