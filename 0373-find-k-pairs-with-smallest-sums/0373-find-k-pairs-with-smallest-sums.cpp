5f23aea2950b638d3b6d9cdb24adec92f073b3f2
Arpit Singh <arpitbuilds@gmail.com>
2026-06-09 15:01:50 +0530

    Time: 99 ms (24.68%), Space: 157.4 MB (28.94%) - LeetHub

--- 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
+++ 0373-find-k-pairs-with-smallest-sums/0373-find-k-pairs-with-smallest-sums.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
-    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {       
-        vector<vector<int>>ans;
+    vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2,
+                                       int k) {
         priority_queue<pair<int,pair<int,int>>>pq;
-        int n=nums1.size();
-        for(int i=0;i<n;i++){
-            for(int j=0;j<nums2.size();j++){
-                int sum=nums1[i]+nums2[j];
-                if(pq.size()<k){
-                    pq.push({sum,{nums1[i],nums2[j]}});
-                }
-                else if(sum<pq.top().first){
+        int m = nums1.size();
+        int n = nums2.size();
+        vector<vector<int>> ans;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                int sum = nums1[i] + nums2[j];
+                if (pq.size() < k) {
+                    pq.push({sum, {nums1[i], nums2[j]}});
+                } else if (pq.top().first > sum) {
                     pq.pop();
-                    pq.push({sum,{nums1[i],nums2[j]}});
-                }
-                else if(sum>=pq.top().first){
+                    pq.push({sum, {nums1[i], nums2[j]}});
+                } else if (sum >= pq.top().first) {
                     break;
                 }
             }
         }
-        while(!pq.empty()){
-            ans.push_back({pq.top().second.first,pq.top().second.second});
+        while (!pq.empty()) {
+            ans.push_back({pq.top().second.first, pq.top().second.second});
             pq.pop();
         }
         return ans;
