679a834de2745a9432bad8d0388aeb35a4779228
Arpit Singh <arpitbuilds@gmail.com>
2026-06-12 21:12:56 +0530

    Time: 16 ms (32.24%), Space: 38.8 MB (11.89%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-
-        unordered_map<int,int> mpp;
-
-        mpp[0] = 1;
-
-        int pf = 0;
-        int cnt = 0;
-
-        for(auto it : nums) {
-
-            pf += it;
-
-            int rem = pf - goal;
-
-            if(mpp.find(rem) != mpp.end()) {
-                cnt += mpp[rem];
+        unordered_map<int,int>mpp;
+        mpp[0]=1;
+        int pf=0;
+        int cnt=0;
+        for(auto it : nums){
+            pf+=it;
+            int rem=pf-goal;
+            if(mpp.find(rem)!=mpp.end()){
+                cnt+=mpp[rem];
             }
-
             mpp[pf]++;
         }
-
         return cnt;
     }
 };
