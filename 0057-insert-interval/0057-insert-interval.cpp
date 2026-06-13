ff223a93ee0f82aee6450dcff9f5ba8233cdaa3a
Arpit Singh <arpitbuilds@gmail.com>
2026-06-13 10:23:13 +0530

    Time: 0 ms (100%), Space: 21.8 MB (45.63%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,28 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> insert(vector<vector<int>>& in, vector<int>& newi) {
-        int n=in.size();
-        vector<vector<int>>ans;
-        int i=0;
-        while(i<n){
-          if(i<n && newi[0]>in[i][1]){
-            ans.push_back(in[i]);
-            i++;
-          }
-          else if(newi[1]<in[i][0]){
-            break;
-          }
-          else{
-             newi[0] = min(newi[0], in[i][0]);
-             newi[1] = max(newi[1], in[i][1]);
+    vector<vector<int>> insert(vector<vector<int>>& inn, vector<int>& newi) {
+        vector<vector<int>> ans;
+        int i = 0;
+        int n = inn.size();
+        while (i < n) {
+            while (i < n && inn[i][1] < newi[0]) {
+                ans.push_back(inn[i]);
                 i++;
-          }
+            }
+            if (i<n && inn[i][0] > newi[1]) {
+                break;
+            } else if(i<n) {
+                newi[0] = min(newi[0], inn[i][0]);
+                newi[1] = max(newi[1], inn[i][1]);
+                i++;
+            }
         }
-         ans.push_back(newi);
-         while(i<n){
-            ans.push_back(in[i]);
+        ans.push_back(newi);
+        while (i < n) {
+            ans.push_back(inn[i]);
             i++;
-         }
+        }
         return ans;
     }
 };
