7c78e1a138e1c708d76b85ecee97eb1b30926bde
Arpit Singh <arpitbuilds@gmail.com>
2026-06-14 02:35:45 +0530

    Time: 11 ms (51.73%), Space: 21.2 MB (30.18%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
-    int expand(string &s,int l,int r){
+    int expand(string s, int l, int r) {
         int cnt = 0;
-
-        while(l>=0 && r<s.size() && s[l]==s[r]){
-            cnt++;
-            l--;
-            r++;
+        while (l >= 0 && r <s.size() && s[l] == s[r]) {
+            
+                cnt++;
+                l--;
+                r++;
+            
+            
         }
-
         return cnt;
     }
-
     int countSubstrings(string s) {
         int ans = 0;
-        int n = s.size();
-
-        for(int i=0;i<n;i++){
-            ans += expand(s,i,i);      // odd length
-            ans += expand(s,i,i+1);    // even length
+        for (int i = 0; i < s.size(); i++) {
+            ans += expand(s, i, i);
+            ans += expand(s, i, i + 1);
         }
-
         return ans;
     }
 };
