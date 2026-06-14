164cbe8377ac283db6736d32034957c700820db4
Arpit Singh <arpitbuilds@gmail.com>
2026-06-15 01:43:46 +0530

    Time: 75 ms (5.43%), Space: 84.4 MB (5.1%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
-    int solve(int i , int j ,string s ,string t,vector<vector<int>>&dp){
-        if(i<0){
-            return j+1;
+    int solve(int i, int j, string s, string t, vector<vector<int>>& dp) {
+        if (j < 0) {
+            return i + 1;
         }
-        if(j<0){
-            return i+1;
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(s[i]==t[j]){
-            return dp[i][j]=0 + solve(i-1,j-1,s,t,dp);
-        }
-        else{
-            return dp[i][j]=1+min({solve(i,j-1,s,t,dp),solve(i-1,j,s,t,dp),solve(i-1,j-1,s,t,dp)});
+        if (i < 0) {
+            return j + 1;
         }
 
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+        if (s[i] == t[j]) {
+            return dp[i][j] = 0 + solve(i - 1, j - 1, s, t, dp);
+        } else {
+            return dp[i][j] = 1 + min({solve(i - 1, j, s, t, dp),
+                                       solve(i, j - 1, s, t, dp),
+                                       solve(i - 1, j - 1, s, t, dp)});
+        }
     }
     int minDistance(string s, string t) {
-        int m=s.size();
-        int n=t.size();
+        int m = s.size();
+        int n = t.size();
         vector<vector<int>> dp(m, vector<int>(n, -1));
-        return solve(m-1,n-1,s,t,dp);
+        return solve(m - 1, n - 1, s, t, dp);
     }
 };
