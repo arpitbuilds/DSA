1c48d6092a4341b7b2b1494964905d3fe7087ba8
Arpit Singh <arpitbuilds@gmail.com>
2026-06-16 02:03:52 +0530

    Time: 0 ms (100%), Space: 11.8 MB (21.9%) - LeetHub

--- 0890-find-and-replace-pattern/0890-find-and-replace-pattern.cpp
+++ 0890-find-and-replace-pattern/0890-find-and-replace-pattern.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
-    bool solve(string word,string p){
-        int m=word.size();
-        int n=p.size();
+    bool solve(string w , string p){
         unordered_map<char,char>wp;
         unordered_map<char,char>pw;
-        for(int i=0;i<n;i++){
-            if(wp.count(word[i]) && wp[word[i]]!=p[i]){
+        for(int i=0;i<p.size();i++){
+            if(wp.count(w[i]) && wp[w[i]]!=p[i]){
                 return false;
             }
-              if(pw.count(p[i]) && pw[p[i]]!=word[i]){
+             if(pw.count(p[i]) && pw[p[i]]!=w[i]){
                 return false;
             }
-            pw[p[i]]=word[i];
-            wp[word[i]]=p[i];
+            wp[w[i]]=p[i];
+            pw[p[i]]=w[i];
         }
         return true;
     }
