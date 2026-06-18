76fff11c821c7f747a45aea475c0c80542c68cf0
Arpit Singh <arpitbuilds@gmail.com>
2026-06-18 16:32:12 +0530

    Time: 50 ms (13.3%), Space: 32.1 MB (79.89%) - LeetHub

---
+++ 0846-hand-of-straights/0846-hand-of-straights.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int gs) {
+        map<int,int>mpp;
+        for(auto it : hand){
+            mpp[it]++;
+        }
+        // if(hand.size()%gs!=0){
+        //     return false;
+        // }
+        sort(hand.begin(),hand.end());
+        for(int i=0;i<hand.size();i++){
+            if(mpp[hand[i]]==0){
+                continue;
+            }
+            for(int j=0;j<gs;j++){
+                int cc=hand[i]+j;
+                if(mpp[cc]==0){
+                    return false;
+                }
+                 mpp[cc]--;
+            }
+           
+        }
+        return true;
+    }
+};
