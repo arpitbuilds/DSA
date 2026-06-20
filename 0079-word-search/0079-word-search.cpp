3d35fab1bcfc02cf4485c9234a58d40490a710ba
Arpit Singh <arpitbuilds@gmail.com>
2026-06-20 14:30:59 +0530

    Time: 820 ms (23.34%), Space: 11.9 MB (13.38%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
-    bool dfs(int i,int j,int ind,int m,int n,vector<vector<char>>&b,string word){
-        int delr[]={0,-1,0,1};
-        int delc[]={-1,0,1,0};
-        if(ind==word.size()){
+    bool dfs(int r, int c, int ind, vector<vector<char>>& b, string word,
+             vector<vector<int>>& vis) {
+
+        if (ind == word.size()) {
             return true;
         }
-        if(i<0 || j<0 || i>=m || j>=n || b[i][j]!=word[ind]){
+        int m = b.size();
+        int n = b[0].size();
+        if (r < 0 || c < 0 || r >= m || c >= n || vis[r][c] == 1 ||
+            b[r][c] != word[ind]) {
             return false;
-
         }
-        char temp=b[i][j];
-        b[i][j]='$';
-        for(int k=0;k<4;k++){
-            int nrow=i+delr[k];
-            int ncol=j+delc[k];
-            if(dfs(nrow,ncol,ind+1,m,n,b,word)){
+        vis[r][c] = 1;
+        int delr[] = {0, -1, 0, 1};
+        int delc[] = {-1, 0, 1, 0};
+        for (int k = 0; k < 4; k++) {
+            int nr = r + delr[k];
+            int nc = c + delc[k];
+            if (dfs(nr, nc, ind + 1, b, word, vis)) {
                 return true;
             }
         }
-        b[i][j]=temp;
+        vis[r][c] = 0;
         return false;
-
     }
     bool exist(vector<vector<char>>& board, string word) {
-        int m=board.size();
-        int n=board[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(board[i][j]==word[0]){
-                    if(dfs(i,j,0,m,n,board,word)){
+        int m = board.size();
+        int n = board[0].size();
+        vector<vector<int>>vis(m, vector<int>(n, 0));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word[0]) {
+                    if (dfs(i, j, 0, board, word, vis)) {
                         return true;
                     }
                 }
