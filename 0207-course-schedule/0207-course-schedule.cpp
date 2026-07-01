ef3805239eb995cebb1b163f0950a64dc18b303f
Arpit Singh <arpitbuilds@gmail.com>
2026-07-02 03:42:55 +0530

    Time: 6 ms (50.07%), Space: 20.2 MB (24.97%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,35 +1,34 @@
 class Solution {
 public:
-    bool canFinish(int n, vector<vector<int>>& pre) {
-       
-        vector<vector<int>>v(n);
-        vector<int>ind(n,0);
-        for(auto it : pre ){
-          v[it[1]].push_back(it[0]);
+    bool canFinish(int num, vector<vector<int>>& p) {
+        vector<vector<int>> adj(num);
+        for (auto it : p) {
+            adj[it[1]].push_back(it[0]);
         }
-        for(int i=0;i<n;i++){
-            for(auto it : v[i]){
+        vector<int> ind(num, 0);
+        for (int i = 0; i < num; i++) {
+            for (auto it : adj[i]) {
                 ind[it]++;
             }
         }
-        queue<int>q;
-        for(int i=0;i<n;i++){
-            if(ind[i]==0){
+        queue<int> q;
+        for (int i = 0; i < num; i++) {
+            if (ind[i] == 0) {
                 q.push(i);
             }
         }
-        int cnt=0;
-        while(!q.empty()){
-            int x=q.front();
+        vector<int> v;
+        while (!q.empty()) {
+            int x = q.front();
             q.pop();
-            cnt++;
-            for(auto it : v[x]){
+            v.push_back(x);
+            for (auto it : adj[x]) {
                 ind[it]--;
-                if(ind[it]==0){
+                if (ind[it] == 0) {
                     q.push(it);
                 }
             }
         }
-        return cnt==n;
+        return v.size()==num;
     }
 };
