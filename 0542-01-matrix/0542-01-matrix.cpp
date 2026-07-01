8b0c766c54a211aa2c8989f11fe8f42205ef4ecc
Arpit Singh <arpitbuilds@gmail.com>
2026-07-01 15:48:05 +0530

    Time: 19 ms (56.06%), Space: 39 MB (12.12%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -4,30 +4,30 @@ public:
         int m = mat.size();
         int n = mat[0].size();
         vector<vector<int>> vis(m, vector<int>(n, 0));
-        vector<vector<int>> dis(m, vector < int > (n, 0));
-        queue<pair<pair<int,int>,int>>q;
+        vector<vector<int>> dis(m, vector<int>(n, 0));
+        queue<pair<pair<int,int>,int>> q;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if(mat[i][j]==0){
-                   q.push({{i,j},0});
-                   vis[i][j]=1;
+                if (mat[i][j] == 0) {
+                    q.push({{i, j}, 0});
+                    vis[i][j]=1;
                 }
             }
         }
-        while(!q.empty()){
+        while (!q.empty()) {
             int r=q.front().first.first;
             int c=q.front().first.second;
             int d=q.front().second;
-            dis[r][c]=d;
             q.pop();
             int delr[]={0,-1,0,1};
             int delc[]={-1,0,1,0};
+            dis[r][c]=d;
             for(int i=0;i<4;i++){
-                int nrow=r+delr[i];
-                int ncol=c+delc[i];
-                if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && !vis[nrow][ncol]){
-                    q.push({{nrow,ncol},d+1});
-                    vis[nrow][ncol]=1;
+                int nr=r+delr[i];
+                int nc=c+delc[i];
+                if(nr>=0 && nc>=0 && nr<m && nc<n && !vis[nr][nc]){
+                    q.push({{nr,nc},d+1});
+                    vis[nr][nc]=1;
                 }
             }
         }
