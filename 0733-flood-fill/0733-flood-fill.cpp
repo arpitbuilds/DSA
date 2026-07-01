6ecea17e16c258a6c4cde1968990ba8125fa9552
Arpit Singh <arpitbuilds@gmail.com>
2026-07-02 04:10:42 +0530

    Time: 0 ms (100%), Space: 17.8 MB (99.93%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    void solve(int inic,int sr,int sc,vector<vector<int>>&img,vector<vector<int>>&ans,int m,int n,int color){
-        ans[sr][sc]=color;
-        int delr[] = {0, -1, 0, 1};
-        int delc[] = {-1, 0, 1, 0};
-        for(int i=0;i<4;i++){
-            int nrow=sr+delr[i];
-            int ncol=sc+delc[i];
-            if(nrow>=0 && ncol>=0 && nrow<m && ncol<n && img[nrow][ncol]==inic && ans[nrow][ncol]!=color){
-                solve(inic,nrow,ncol,img,ans,m,n,color);
+    void solve(int r,int c,vector<vector<int>>&img,int color,int inic){
+        int m=img.size();
+        int n=img[0].size();
+        img[r][c]=color;
+        int delr[]={0,-1,0,1};
+        int delc[]={-1,0,1,0};
+        for(int k=0;k<4;k++){
+            int nr=r+delr[k];
+            int nc=c+delc[k];
+            if(nr>=0 && nc>=0 && nr<m && nc<n && img[nr][nc]==inic && img[nr][nc]!=color){
+                solve(nr,nc,img,color,inic);
             }
         }
-
     }
     vector<vector<int>> floodFill(vector<vector<int>>& img, int sr, int sc, int color) {
         int m=img.size();
         int n=img[0].size();
         int inic=img[sr][sc];
-        vector<vector<int>>ans=img;
-        solve(inic,sr,sc,img,ans,m,n,color);
-        return ans;
+        solve(sr,sc,img,color,inic);
+        return img;
     }
 };
