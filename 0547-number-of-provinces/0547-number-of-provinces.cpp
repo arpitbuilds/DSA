51528c702839cf6ae5d5b8cfc45a43eb274ff6d0
Arpit Singh <arpitbuilds@gmail.com>
2026-07-01 13:48:29 +0530

    Time: 3 ms (37.32%), Space: 21.1 MB (9.48%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    void solve(int i,vector<int>&vis,vector<vector<int>>&adj,int &cnt){
+    void solve(int i,vector<int>&vis,vector<vector<int>>&adj){
         vis[i]=1;
         for(auto it : adj[i]){
             if(!vis[it]){
-                solve(it,vis,adj,cnt);
+                solve(it,vis,adj);
             }
         }
     }
     int findCircleNum(vector<vector<int>>& is) {
         int n=is.size();
-        vector<vector<int>>v(n);
-        vector<int>vis(n);
-        int cnt=0;
+        vector<vector<int>>adj(n);
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(is[i][j]==1 && i!=j){
-                    v[i].push_back(j);
-                    v[j].push_back(i);
+                if(is[i][j]==1){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
                 }
             }
         }
+        vector<int>vis(n,0);
+        int cnt=0;
         for(int i=0;i<n;i++){
             if(!vis[i]){
-                  cnt++;
-                solve(i,vis,v,cnt);
+                cnt++;
+                solve(i,vis,adj);
             }
         }
         return cnt;
