d3ec5009a3ec015fee8b7c2bb67cc6d34008fc58
Arpit Singh <arpitbuilds@gmail.com>
2026-07-02 17:33:41 +0530

    Time: 11 ms (28.16%), Space: 37.2 MB (25.74%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -6,32 +6,42 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void solve(TreeNode*root,vector<vector<int>>&ans,vector<int>&path,int target){
-        if(root==NULL){
-            return;
+    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+        vector<vector<int>> ans;
+        if (root == NULL) {
+            return ans;
         }
-        path.push_back(root->val);
-        target-=root->val;
-        if(!root->left && !root->right){
-            if(target==0){
-                ans.push_back(path);
+        queue < pair<TreeNode*, pair<int, vector<int>>>> q;
+        vector<int> temp;
+        temp.push_back(root->val);
+        q.push({root,{root->val,temp}});
+        while (!q.empty()) {
+            TreeNode* x = q.front().first;
+            int sum = q.front().second.first;
+            vector<int> v = q.front().second.second;
+            q.pop();
+            if (x->left == NULL && x->right == NULL) {
+                if (sum == targetSum) {
+        ans.push_back(v);
+    }
+            }
+            if (x->left) {
+                vector<int> left = v;
+                left.push_back(x->left->val);
+                q.push({x->left, {sum + x->left->val, left}});
+            }
+            if (x->right) {
+                vector<int> right = v;
+                right.push_back(x->right->val);
+                q.push({x->right, {sum + x->right->val, right}});
             }
         }
-       
-        solve(root->left,ans,path,target);
-        solve(root->right,ans,path,target);
-        path.pop_back();
-    }
-    vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-         vector<vector<int>>ans;
-        vector<int>path;
-
-        solve(root,ans,path,targetSum);
         return ans;
     }
 };
