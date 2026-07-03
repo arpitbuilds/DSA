5024731edf1225c837e2c397000b1babd668ea8c
Arpit Singh <arpitbuilds@gmail.com>
2026-07-04 00:28:14 +0530

    Time: 0 ms (100%), Space: 19.7 MB (75%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -6,40 +6,39 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if (root == NULL) {
-            return 0;
-        }
-        queue<pair<TreeNode*, unsigned long long>> q;
-        q.push({root, 0});
-        int maxi = INT_MIN;
-        unsigned long long left, right;
-        while (!q.empty()) {
-            int s = q.size();
-            for (int i = 0; i < s; i++) {
-                TreeNode* x = q.front().first;
-                unsigned long long ind = q.front().second;
-                q.pop();
-                if (x->left) {
-                    q.push({x->left, 2 * ind + 1});
-                }
-                if (x->right) {
-                    q.push({x->right, 2 * ind + 2});
-                }
-                if (i == 0) {
-                    left = ind;
-                }
-                if (i == s - 1) {
-                    right = ind;
-                }
+        queue<pair<TreeNode*,unsigned long long>>q;
+        q.push({root,0});
+        unsigned long long left;
+        unsigned long long right;
+        unsigned long long maxi=0;
+        while(!q.empty()){
+            int s=q.size();
+            for(int i=0;i<s;i++){
+            TreeNode*x=q.front().first;
+            unsigned long long ind=q.front().second;
+            q.pop();
+            if(x->left){
+                q.push({x->left,2*ind+1});
             }
-            maxi = max(maxi, (int)(right - left + 1));
+            if(x->right){
+                q.push({x->right,2*ind+2});
+            }
+            if(i==0){
+                left=ind;
+            }
+            if(i==s-1){
+                right=ind;
+            }
+            
+            }
+            maxi=max(maxi,right-left+1);
+            
         }
         return maxi;
     }
