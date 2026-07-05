069cdfd268c77af7d3611aed9560312def676e7f
Arpit Singh <arpitbuilds@gmail.com>
2026-07-05 16:14:56 +0530

    Time: 0 ms (100%), Space: 15.7 MB (28.07%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.cpp
@@ -15,15 +15,15 @@ public:
             return head;
         }
         ListNode*evenh=head->next;
-        ListNode*even=head->next;
         ListNode*odd=head;
-        while(odd && odd->next && even &&  even->next){
-            odd->next=odd->next->next;
-            odd=odd->next;
-            even->next=even->next->next;
-            even=even->next;
+        ListNode*even=head->next;
+        while(odd && odd->next &&  even && even->next){
+           odd->next=odd->next->next;
+           odd=odd->next;
+           even->next=even->next->next;
+           even=even->next;
         }
-       odd->next=evenh;
-       return head;
+        odd->next=evenh;
+        return head;
     }
 };
