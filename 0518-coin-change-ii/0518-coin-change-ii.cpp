6e0cba257d25cd1f6d23a97230cc5e7cdb2b398f
Arpit Singh <arpitbuilds@gmail.com>
2026-07-06 21:42:53 +0530

    Time: 28 ms (55.51%), Space: 36.6 MB (55.35%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(int ind,int amt,vector<int>&coins,vector<vector<int>>&dp){
+    int solve(int ind,vector<int>&coins,int amt,vector<vector<int>>&dp){
         if(ind==0){
             if(amt%coins[ind]==0){
                 return 1;
@@ -12,16 +12,17 @@ public:
         if(dp[ind][amt]!=-1){
             return dp[ind][amt];
         }
-        int ntake=solve(ind-1,amt,coins,dp);
+        int ntake=solve(ind-1,coins,amt,dp);
         int take=0;
         if(coins[ind]<=amt){
-            take=solve(ind,amt-coins[ind],coins,dp);
+            take=solve(ind,coins,amt-coins[ind],dp);
         }
         return dp[ind][amt]=take+ntake;
     }
     int change(int amount, vector<int>& coins) {
         int n=coins.size();
         vector<vector<int>>dp(n,vector<int>(amount+1,-1));
-        return solve(n-1,amount,coins,dp);
+        int ans=solve(n-1,coins,amount,dp);
+        return ans;
     }
 };
