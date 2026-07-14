d08e9526db2ea635c57c238eea9743520b1d1467
Arpit Singh <arpitbuilds@gmail.com>
2026-07-14 15:42:46 +0530

    Time: 4 ms (14.82%), Space: 22.9 MB (52.07%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int candy(vector<int>& r) {
+        int n=r.size();
+        vector<int>ans(n,1);
+        for(int i=1;i<n;i++){
+            if(r[i]>r[i-1] && ans[i-1]>=ans[i]){
+                ans[i]=ans[i-1]+1;
+            }
+        }
+        for(int i=n-2;i>=0;i--){
+            if(r[i+1]<r[i] && ans[i+1]>=ans[i]){
+                ans[i]=ans[i+1]+1;
+            }
+        }
         int sum=0;
-        int n = r.size();
-        vector<int> ans(n, 1);
-        for (int i = 1; i < n; i++) {
-            if (r[i - 1] < r[i] && ans[i - 1] >= ans[i]) {
-                ans[i] = ans[i - 1] + 1;
-            }
-        }
-        for (int i = n - 2; i >= 0; i--) {
-            if (r[i + 1] < r[i] && ans[i + 1] >= ans[i]) {
-                ans[i] = ans[i + 1] + 1;
-            }
-        }
         for(auto it : ans){
             sum+=it;
         }
