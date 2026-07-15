61d94d4cc1dd25fa1372eef6ad32026e33330424
Arpit Singh <arpitbuilds@gmail.com>
2026-07-16 00:03:24 +0530

    Time: 0 ms (100%), Space: 20 MB (49.21%) - LeetHub

---
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool checkXMatrix(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==j || j==n-1-i){
+                    if(grid[i][j]==0){
+                        return false;
+                    }
+                }
+                else{
+                    if(grid[i][j]!=0){
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+};
