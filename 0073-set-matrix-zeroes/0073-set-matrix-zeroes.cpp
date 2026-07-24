5b254480fc29a8668cd61c095759e065bb279703
Arpit Singh <arpitbuilds@gmail.com>
2026-07-24 16:52:26 +0530

    Time: 3 ms (28.85%), Space: 21.1 MB (21.16%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -3,8 +3,8 @@ public:
     void setZeroes(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
-        set<int> r;
-        set<int> c;
+        unordered_set<int> r;
+        unordered_set<int> c;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (mat[i][j] == 0) {
