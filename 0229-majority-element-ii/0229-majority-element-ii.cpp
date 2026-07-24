706cf4e670d931ec58bb9d2e023f40133e748737
Arpit Singh <arpitbuilds@gmail.com>
2026-07-24 21:00:15 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int cnt1 = 0;
-        int cnt2 = 0;
-        int ele1 = 0;
-        int ele2 = 0;
-        int n =nums.size();
-        for (auto it : nums) {
-            if (it == ele1) {
+        int cnt1=0;
+        int cnt2=0;
+        int a=0;
+        int b=0;
+        int n=nums.size();
+        for(auto it : nums){
+            if(a==it){
                 cnt1++;
-            } else if (it == ele2) {
+            }
+            else if(b==it){
                 cnt2++;
-            } else if (cnt1 == 0) {
-                cnt1++;
-                ele1 = it;
-            } else if (cnt2 == 0) {
-                cnt2++;
-                ele2 = it;
-            } else {
+            }
+            else if(cnt1==0){
+                cnt1=1;
+                a=it;
+            }
+            else if(cnt2==0){
+                cnt2=0;
+                b=it;
+            }
+            else{
                 cnt1--;
                 cnt2--;
             }
@@ -25,19 +29,19 @@ public:
         cnt1=0;
         cnt2=0;
         for(auto it : nums){
-            if(it==ele1){
+            if(it==a){
                 cnt1++;
             }
-            if(it==ele2){
+            else if(it==b){
                 cnt2++;
             }
         }
         vector<int>ans;
         if(cnt1>n/3){
-            ans.push_back(ele1);
+            ans.push_back(a);
         }
-        if(cnt2>n/3 && ele1!=ele2){
-            ans.push_back(ele2);
+        if(cnt2>n/3 && a!=b){
+            ans.push_back(b);
         }
         return ans;
     }
