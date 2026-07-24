5e2f4692cf5b83a97f83e1653f8cb4dfd2e4dc46
Arpit Singh <arpitbuilds@gmail.com>
2026-07-24 22:22:10 +0530

    Time: 0 ms (100%), Space: 18.4 MB (66.85%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -11,13 +11,16 @@
  */
 class Solution {
 public:
-      bool solve(TreeNode* p, TreeNode* q){
+    bool solve(TreeNode*p,TreeNode*q){
         if(p==NULL || q==NULL){
             return p==q;
         }
         return (p->val==q->val)&&solve(p->left,q->right)&&solve(p->right,q->left);
     }
     bool isSymmetric(TreeNode* root) {
+        if(root==NULL){
+            return true;
+        }
         return solve(root,root);
     }
 };
