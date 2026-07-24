c18f711e418a354cb2d09b341a60a3a6b6183d81
Arpit Singh <arpitbuilds@gmail.com>
2026-07-25 01:14:04 +0530

    Time: 11 ms (27.86%), Space: 37.1 MB (25.89%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -6,40 +6,39 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
- * right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<vector<int>> ans;
-        if (root == NULL) {
+        vector<vector<int>>ans;
+        if(root==NULL){
             return ans;
         }
-        queue < pair<TreeNode*, pair<int, vector<int>>>> q;
-        vector<int> temp;
+        queue<pair<TreeNode*,pair<int,vector<int>>>>q;
+        vector<int>temp;
         temp.push_back(root->val);
         q.push({root,{root->val,temp}});
-        while (!q.empty()) {
-            TreeNode* x = q.front().first;
-            int sum = q.front().second.first;
-            vector<int> v = q.front().second.second;
+        while(!q.empty()){
+            TreeNode*x=q.front().first;
+            int sum=q.front().second.first;
+            vector<int>v=q.front().second.second;
             q.pop();
-            if (x->left == NULL && x->right == NULL) {
-                if (sum == targetSum) {
-        ans.push_back(v);
-    }
+            if(x->left==NULL && x->right==NULL){
+                if(targetSum==sum){
+                ans.push_back(v);
+                }
             }
-            if (x->left) {
-                vector<int> left = v;
-                left.push_back(x->left->val);
-                q.push({x->left, {sum + x->left->val, left}});
+            if(x->left){
+                vector<int>left=v;
+                left.push_back({x->left->val});
+                q.push({x->left,{sum+x->left->val,left}});
             }
-            if (x->right) {
-                vector<int> right = v;
-                right.push_back(x->right->val);
-                q.push({x->right, {sum + x->right->val, right}});
+            if(x->right){
+                vector<int>right=v;
+                right.push_back({x->right->val});
+                q.push({x->right,{sum+x->right->val,right}});
             }
         }
         return ans;
