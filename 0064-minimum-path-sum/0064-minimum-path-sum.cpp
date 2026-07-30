a659d4d3f71c65929f1798d22d0f94fd840bc3bb
Arpit Singh <arpitbuilds@gmail.com>
2026-07-30 13:02:23 +0530

    Time: 0 ms (100%), Space: 16.4 MB (42.96%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int solve(int i, int j, vector<vector<int>>& g, vector<vector<int>>& dp) {
-        if (i < 0 || j < 0) {
+         if(i<0 || j<0){
             return INT_MAX;
         }
-        if (i == 0 && j == 0) {
+        if(i==0 && j==0){
             return g[i][j];
         }
+      
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
-        int op1=solve(i-1,j,g,dp);
-        int op2=solve(i,j-1,g,dp);
-        return dp[i][j]=g[i][j]+min(op1,op2);
+       int op1=solve(i-1,j,g,dp);
+       int op2=solve(i,j-1,g,dp);
+       return dp[i][j]=g[i][j]+min(op1,op2);
     }
     int minPathSum(vector<vector<int>>& g) {
         int m = g.size();
