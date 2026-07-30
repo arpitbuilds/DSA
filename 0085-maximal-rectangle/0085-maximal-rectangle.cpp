95353f07aedc88e0fabd9ad037d8cf020a9336ea
Arpit Singh <arpitbuilds@gmail.com>
2026-07-30 08:39:40 +0530

    Time: 8 ms (54.69%), Space: 22.7 MB (23.08%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.cpp
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -1,52 +1,50 @@
 class Solution {
 public:
-      int solve(vector<int>& h) {
-        stack<int>st;
-        stack<int>st2;
-        int n=h.size();
-        vector<int>pse(n);
-        vector<int>nse(n);
-        for(int i=n-1;i>=0;i--){
-            while(!st.empty() && h[st.top()]>=h[i]){
+    int solve(vector<int>& h) {
+        stack<int> st;
+        stack<int> st2;
+        int n = h.size();
+        vector<int> pse(n);
+        vector<int> nse(n);
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && h[st.top()] >= h[i]) {
                 st.pop();
             }
-            nse[i]=st.empty()?n:st.top();
+            nse[i] = st.empty() ? n : st.top();
             st.push(i);
-
         }
-        for(int i=0;i<n;i++){
-            while(!st2.empty() && h[st2.top()]>=h[i]){
+        for (int i = 0; i < n; i++) {
+            while (!st2.empty() && h[st2.top()] >= h[i]) {
                 st2.pop();
             }
-            pse[i]=st2.empty()?-1:st2.top();
+            pse[i] = st2.empty() ? -1 : st2.top();
             st2.push(i);
         }
-        int maxi=0;
-        for(int i=0;i<n;i++){
-            int left=pse[i];
-            int right=nse[i];
-            int w=right-left-1;
-            maxi=max(maxi,w*h[i]);
+        int maxi = 0;
+        for (int i = 0; i < n; i++) {
+            int left = pse[i];
+            int right = nse[i];
+            int w = right - left - 1;
+            maxi = max(maxi, w * h[i]);
         }
         return maxi;
     }
 
     int maximalRectangle(vector<vector<char>>& mat) {
-      int m=mat.size();
-      int maxi=INT_MIN;
-      int n=mat[0].size();
-      vector<int>v(n,0);
-      for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(mat[i][j]=='1'){
-                v[j]++;
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<int>v(n,0);
+        int maxi=INT_MIN;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == '1') {
+                    v[j]++;
+                } else {
+                    v[j] = 0;
+                }
             }
-            else{
-                v[j]=0;
-            }
+            maxi=max(maxi,solve(v));
         }
-        maxi=max(maxi,solve(v));
-      }
-      return maxi;
+        return maxi;
     }
 };
