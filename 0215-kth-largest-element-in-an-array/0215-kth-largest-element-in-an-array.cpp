a11740e2285f0581f917eeef2bc9792b23ee8819
Arpit Singh <arpitbuilds@gmail.com>
2026-07-31 07:20:34 +0530

    Time: 36 ms (52.11%), Space: 72.5 MB (39.64%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -4,11 +4,10 @@ public:
         priority_queue<int,vector<int>,greater<int>>pq;
         for(auto it : nums){
             pq.push(it);
-         if(pq.size()>k){
-            pq.pop();
+            if(pq.size()>k){
+                pq.pop();
+            }
         }
-        }
-       
         return pq.top();
     }
 };
