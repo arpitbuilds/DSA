a65d76292af3a31c37fa7aa898ffb40e2c37eea2
Arpit Singh <arpitbuilds@gmail.com>
2026-07-31 08:12:21 +0530

    Time: 8 ms (59.76%), Space: 71.4 MB (95.44%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
-    int solve(vector<int>&nums,int k){
-        if(k<0){
+    int solve(vector<int>& nums, int k) {
+        if (k < 0) {
             return 0;
         }
-        int ans=0;
-        int l=0;
-        int r=0;
+        int ans = 0;
         int cnt=0;
-        int n=nums.size();
-        while(r<n){
-            if(nums[r]%2!=0){
-               cnt++;
+        int l = 0;
+        int n = nums.size();
+        int r = 0;
+        while (r < n) {
+            if (nums[r] % 2 == 1) {
+                cnt++;
             }
-            while(cnt>k){
-                if(nums[l]%2!=0){
+            while (cnt > k) {
+                if (nums[l] % 2 == 1) {
                     cnt--;
                 }
+                
                 l++;
             }
-            ans+=r-l+1;
+            ans += r - l + 1;
             r++;
         }
         return ans;
