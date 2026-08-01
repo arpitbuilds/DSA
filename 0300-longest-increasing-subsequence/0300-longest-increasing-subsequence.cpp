286572455288033806812f4d96d4b4e239fdca75
Arpit Singh <arpitbuilds@gmail.com>
2026-08-01 15:45:56 +0530

    Time: 553 ms (18.66%), Space: 296.3 MB (27.65%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int solve(int ind,vector<int>&nums,int prev,int n,vector<vector<int>>&dp){
+    int solve(int ind,int n,vector<int>&nums,int &prev,vector<vector<int>>&dp){
         if(ind==n){
             return 0;
         }
         if(dp[ind][prev+1]!=-1){
             return dp[ind][prev+1];
         }
-        int ntake=0+solve(ind+1,nums,prev,n,dp);
+        int ntake=0+solve(ind+1,n,nums,prev,dp);
         int take=0;
         if(prev==-1 || nums[ind]>nums[prev]){
-            take=1+solve(ind+1,nums,ind,n,dp);
+           take=1+solve(ind+1,n,nums,ind,dp);
         }
         return dp[ind][prev+1]=max(take,ntake);
     }
@@ -18,7 +18,6 @@ public:
         int n=nums.size();
         int prev=-1;
         vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return solve(0,nums,prev,n,dp);
-        
+        return solve(0,n,nums,prev,dp);
     }
 };
