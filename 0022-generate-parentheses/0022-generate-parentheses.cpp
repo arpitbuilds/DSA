a3422faea779dc24c73886f08d162db75bfa59a8
Arpit Singh <arpitbuilds@gmail.com>
2026-08-02 00:48:38 +0530

    Time: 3 ms (69.49%), Space: 15.7 MB (53.63%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    void solve(int oc,int cc,string t,vector<string>&ans,int n){
-        if(oc==n && cc==n){
-            ans.push_back(t);
-            return ;
-        }
-        if(oc<n){
-            solve(oc+1,cc,t+'(',ans,n);
-        }
-        if(cc<oc){
-            solve(oc,cc+1,t+')',ans,n);
-        }
+    void solve(int oc,int cc,int n,string s,vector<string>&v){
+       if(oc==n && cc==n){
+        v.push_back(s);
+        return ;
+       } 
+       if(oc<n){
+        solve(oc+1,cc,n,s+"(",v);
+       }
+       if(cc<oc){
+        solve(oc,cc+1,n,s+")",v);
+       }
     }
     vector<string> generateParenthesis(int n) {
+        string s="";
+        vector<string>v;
         int oc=0;
         int cc=0;
-        string temp="";
-        vector<string>ans;
-        solve(oc,cc,temp,ans,n);
-        return ans;
+        solve(oc,cc,n,s,v);
+        return v;
     }
 };
