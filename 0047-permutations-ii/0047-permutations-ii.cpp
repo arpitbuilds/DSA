06865a0f3de9f242abc7c8731d7d9c5ea47fa81d
Arpit Singh <arpitbuilds@gmail.com>
2026-08-02 02:28:45 +0530

    Time: 2 ms (71.3%), Space: 12 MB (62.69%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.cpp
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -1,32 +1,31 @@
 class Solution {
 public:
-    void solve(int ind, vector<int>& v, vector<vector<int>>& ans,
-               vector<int>& nums, vector<int>& freq) {
+    vector<vector<int>> ans;
+    void solve(int ind, vector<int>& v, vector<int>& vis, vector<int>& nums) {
+
         if (ind == nums.size()) {
             ans.push_back(v);
             return;
         }
         for (int i = 0; i < nums.size(); i++) {
-            if (freq[i] != 1) {
-                freq[i] = 1;
+            if (!vis[i]) {
                 v.push_back(nums[i]);
-                solve(ind + 1, v, ans, nums, freq);
-                freq[i] = 0;
-                while (i + 1 < nums.size() && nums[i] == nums[i + 1]) {
-                    i++;
+                vis[i] = 1;
+                solve(ind + 1, v, vis, nums);
+                 vis[i] = 0;
+                while(i+1<nums.size() && nums[i]==nums[i+1]){
+                     i++;
                 }
                 v.pop_back();
-
-                
             }
         }
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> v;
-        sort(nums.begin(), nums.end());
-        vector<int> freq(nums.size(), 0);
-        solve(0, v, ans, nums, freq);
+        vector<int>v;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        vector<int>vis(n,0);
+        solve(0,v,vis,nums);
         return ans;
     }
 };
