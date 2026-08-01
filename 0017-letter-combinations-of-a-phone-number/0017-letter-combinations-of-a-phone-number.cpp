4920ba3b767e8309727d5e28d817c40bd99f0654
Arpit Singh <arpitbuilds@gmail.com>
2026-08-02 00:39:32 +0530

    Time: 0 ms (100%), Space: 9.2 MB (62.7%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
-    void solve(int ind,string s,vector<string>&v,string d,unordered_map<char,string>&mpp){
-        if(ind==d.size()){
+    void solve(int ind,string s,vector<string>&v,string digi,unordered_map<char,string>&mpp){
+        if(ind==digi.size()){
             v.push_back(s);
             return;
         }
-        char x=d[ind];
-        string word=mpp[x];
-        for(int i=0;i<word.size();i++){
-            s+=word[i];
-            solve(ind+1,s,v,d,mpp);
+        char x=digi[ind];
+        string k=mpp[x];
+        int n=k.size();
+        for(int i=0;i<n;i++){
+            s.push_back(k[i]);
+            solve(ind+1,s,v,digi,mpp);
             s.pop_back();
         }
     }
     vector<string> letterCombinations(string digits) {
-        unordered_map<char,string>mpp;
-        mpp['2'] = "abc";
-        mpp['3'] = "def";
-        mpp['4'] = "ghi";
-        mpp['5'] = "jkl";
-        mpp['6'] = "mno";
-        mpp['7'] = "pqrs";
-        mpp['8'] = "tuv";
-        mpp['9'] = "wxyz";
         vector<string>v;
         string s="";
+        unordered_map<char,string>mpp;
+        mpp['2']="abc";
+        mpp['3']="def";
+        mpp['4']="ghi";
+        mpp['5']="jkl";
+        mpp['6']="mno";
+        mpp['7']="pqrs";
+        mpp['8']="tuv";
+        mpp['9']="wxyz";
         solve(0,s,v,digits,mpp);
         return v;
     }
