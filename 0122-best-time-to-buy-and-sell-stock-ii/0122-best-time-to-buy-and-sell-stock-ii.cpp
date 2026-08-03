f7a6bc27ea4236988cc87f1a11a0440f610ab47e
Arpit Singh <arpitbuilds@gmail.com>
2026-08-03 06:37:05 +0530

    Time: 15 ms (14.87%), Space: 27.9 MB (6.52%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,39 +1,25 @@
 class Solution {
 public:
-    // int solve(int ind ,int b,vector<int>&p,vector<vector<int>>&dp,int n){
-    //     if(ind==n){
-    //         return 0;
-    //     }
-    //     if(dp[ind][b]!=-1){
-    //         return dp[ind][b];
-    //     }
-    //     int profit=0;
-    //     if(b){
-    //     profit=max((-p[ind]+solve(ind+1,0,p,dp,n)),(0+solve(ind+1,1,p,dp,n)));
-    //     }
-    //     else{
-    //         profit=max((p[ind]+solve(ind+1,1,p,dp,n)),(0+solve(ind+1,0,p,dp,n)));
-    //     }
-    //     return dp[ind][b]=profit;
-    // }
-    int maxProfit(vector<int>& p) {
-        int n=p.size();
-        vector<vector<int>>dp(n+1,vector<int>(2,0));
-        // return solve(0,1,prices,dp,n);
-        dp[n][0]=0;
-        dp[n][1]=0;
-        for(int i=n-1;i>=0;i--){
-            for(int b=0;b<2;b++){
-                  int profit=0;
-         if(b){
-        profit=max((-p[i]+dp[i+1][0]),(0+dp[i+1][1]));
+    int solve(int ind,int b,int n,vector<int>&prices,vector<vector<int>>&dp){
+        if(ind==prices.size()){
+            return 0;
+        }
+        int p=0;
+        if(dp[ind][b]!=-1){
+            return dp[ind][b];
+        }
+        if(b){
+            p=max((-prices[ind]+solve(ind+1,0,n,prices,dp)),(0+solve(ind+1,b,n,prices,dp)));
         }
         else{
-            profit=max((p[i]+dp[i+1][1]),(0+dp[i+1][0]));
+            p=max((prices[ind]+solve(ind+1,1,n,prices,dp)),(0+solve(ind+1,b,n,prices,dp)));
         }
-         dp[i][b]=profit;
-            }
-        }
-        return dp[0][1];
+        return  dp[ind][b]=p;
+    }
+    int maxProfit(vector<int>& prices) {
+        int b=1;
+        int n=prices.size();
+        vector<vector<int>>dp(n,vector<int>(2,-1));
+        return solve(0,b,n,prices,dp);
     }
 };
