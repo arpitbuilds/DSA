2e9208c2b4626e7f0508a209e0cf576fbdc00b75
Arpit Singh <arpitbuilds@gmail.com>
2026-09-21 00:34:14 +0530

    Time: 15 ms (81.54%), Space: 48 MB (87.8%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -0,0 +1,72 @@
+class LRUCache {
+public:
+    class Node{
+        public:
+        int key;
+        int val;
+        Node*next;
+        Node*prev;
+        Node(int k,int v){
+            key=k;
+            val=v;
+        }
+    };
+    int cap;
+    Node*head=new Node(-1,-1);
+    Node*tail=new Node(-1,-1);
+    unordered_map<int,Node*>mpp;
+    LRUCache(int capacity) {
+        cap=capacity;
+        head->next=tail;
+        tail->prev=head;
+    }
+    void insertatfront(Node*node){
+        Node*temp=head->next;
+        head->next=node;
+        temp->prev=node;
+        node->next=temp;
+        node->prev=head;
+    }
+    void del(Node*node){
+        Node*front=node->next;
+        Node*previous=node->prev;
+        previous->next=front;
+        front->prev=previous;
+    }
+    int get(int key) {
+        if(mpp.find(key)!=mpp.end()){
+            Node*node=mpp[key];
+            del(node);
+            insertatfront(node);
+            return node->val;
+        }
+        return -1;
+    }
+    
+    void put(int key, int value) {
+        if(mpp.find(key)!=mpp.end()){
+            Node*node=mpp[key];
+            node->val=value;
+            del(node);
+            insertatfront(node);
+        }
+        else{
+            if(mpp.size()==cap){
+                Node*lru=tail->prev;
+                del(lru);
+                mpp.erase(lru->key);
+                delete lru;
+            }
+            Node*node=new Node(key,value);
+            insertatfront(node);
+            mpp[key]=node;
+        }
+    }
+};
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
